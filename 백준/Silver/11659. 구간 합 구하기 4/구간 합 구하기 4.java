c63d1ac607523d110092eb639b56e07d43ba6e28
gomie1 <nagum0545@gmail.com>
2024-08-24 14:42:06 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1116 ms, Memory: 58932 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,33 +10,20 @@ public class Main {
         int N = Integer.parseInt(st.nextToken()); // 수의 개수
         int M = Integer.parseInt(st.nextToken()); // 합을 구해야하는 횟수
 
-        // N개의 숫자 입력 받기
-        int[] arr = new int[N];
-        int[] pSum = new int[N+1]; // 누적합 배열
+        // 누적합 저장
+        int[] arr = new int[N+1];
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-
-            /* 누적합 구하기 */
-            if(i == 0) {
-                continue;
-            }
-
-            if(i == 1) {
-                pSum[i] = arr[i-1];
-            }
-            else {
-                pSum[i] = pSum[i-1] + arr[i-1];
-            }
+        arr[0] = 0;
+        for(int i = 1; i <= N; i++) {
+            arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
         }
-        pSum[N] = pSum[N-1] + arr[N-1];
 
         for(int a = 0; a < M; a++) {
             st = new StringTokenizer(br.readLine());
             int i = Integer.parseInt(st.nextToken());
             int j = Integer.parseInt(st.nextToken());
 
-            System.out.println(pSum[j] - pSum[i-1]);
+            System.out.println(arr[j] - arr[i-1]);
         }
     }
 }
