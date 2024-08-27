b4fc19f9ebd8d69bb3b3a75b3604b06ab7ddfd31
gomie1 <nagum0545@gmail.com>
2024-08-27 22:19:55 +0900

    [Silver II] Title: DFS와 BFS, Time: 228 ms, Memory: 20648 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -4,86 +4,84 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M, V;
-    static List<List<Integer>> graph;
-    static boolean[] visited;
-    static boolean[] visited2;
-    static StringBuilder sb = new StringBuilder();
+	
+	static ArrayList<Integer>[] graph;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken()); // 정점의 개수
+		int M = Integer.parseInt(st.nextToken()); // 간선의 개수
+		int V = Integer.parseInt(st.nextToken()); // 시작 정점 번호
+		
+		// ArrayList 2차원 배열 선언
+		graph = new ArrayList[N+1];
+		for(int i = 1; i <= N; i++) {
+			graph[i] = new ArrayList<Integer>();
+		}
+		
+		// 정점들 연결 (양방향)
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			graph[a].add(b);
+			graph[b].add(a);
+		}
+		
+		// 방문할 수 있는 정점이 여러 개인 경우 정점 번호가 작은 것부터 방문하기 위해 그래프 정렬
+		for(int i = 1; i <= N; i++) {
+			Collections.sort(graph[i]);
+		}
+		
+		visited = new boolean[N+1];
+		dfs(V);
+		sb.append("\n");
+		
+		visited = new boolean[N+1];
+		bfs(V);
+		System.out.println(sb);
+	}
 
-        N = Integer.parseInt(st.nextToken()); // 정점의 개수
-        M = Integer.parseInt(st.nextToken()); // 노드의 개수
-        V = Integer.parseInt(st.nextToken()); // 탐색을 시작할 정점의 번호
-
-        graph = new ArrayList<>(); // 배열 하나 생성
-        for(int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<>()); // 생성한 배열 안에 원소로 배열 하나씩 추가
-        }
-
-	    // M개의 줄에 주어지는 두 정점의 번호 입력 받기
-	    for(int i = 0; i < M; i++) {
-	        String[] nv = br.readLine().split(" "); // 한 줄로 입력받고 띄어쓰기를 기준으로 분리해서 배열에 담기
-	        int n1 = Integer.parseInt(nv[0]);
-	        int n2 = Integer.parseInt(nv[1]);
-	        
-	        // 양방향 연결
-	        graph.get(n1).add(n2);
-	        graph.get(n2).add(n1);
-	    }
-	    
-	    for(int i = 0; i < graph.size(); i++) {
-	    	Collections.sort(graph.get(i));
-	    }
-
-	    visited = new boolean[N+1]; // 방문표시 배열
-	    dfs(V);
-	    sb.append("\n");
-	    
-	    visited2 = new boolean[N+1];
-	    bfs(V);
-	    System.out.println(sb);
+	private static void dfs(int cur) {
+		visited[cur] = true; // 현재 정점 방문처리
+		sb.append(cur + " ");
+		
+		for(int vertex : graph[cur]) { // 현재 정점과 연결된 정점들에 대해 하나씩 순회
+			if(!visited[vertex]) { // 연결된 정점에 방문한 적이 없다면
+				dfs(vertex); // 재귀를 통해 해당 정점으로 이동
+			}
+		}
 	}
 	
-	/* DFS */
-	public static void dfs(int start) {
-	    visited[start] = true; // 시작 노드 방문 처리
-	    sb.append(start + " ");
-	    
-	    for(int node : graph.get(start)) { // 시작 노드에 연결된 노드들에 대해 하나씩 순회
-	        if(!visited[node]) { // 현재 노드에 방문한 적이 없다면
-	            dfs(node);
-	        }
-	    }
-	}
-	
-	/* BFS */
-	public static void bfs(int start) {
-	    Queue<Integer> queue = new LinkedList<Integer>(); // 큐 생성
-	    queue.add(start); // 시작 노드를 큐에 넣어줌
-	    visited2[start] = true; // 시작 노드 방문 처리
-	    
-	    // 큐가 비지 않을 때까지 동작
-	    while(!queue.isEmpty()) {
-	    	int node = queue.poll(); // 현재 노드를 큐에서 꺼내서 node에 담아줌
-	    	sb.append(node + " ");
-	    	
-	    	// 현재 노드에 연결된 노드 수만큼 반복
-	    	for(int i = 0; i < graph.get(node).size(); i++) {
-	    		int nn = graph.get(node).get(i); // 연결된 노드를 nn에 담아줌
-	    		
-	    		if(!visited2[nn]) { // 연결된 노드에 방문한 적이 없다면
-	    			visited2[nn] = true; // 방문 처리한 후
-	    			queue.add(nn); // 큐에 담아줌
-	    		}
-	    	}
-	    }
+	private static void bfs(int start) {
+		Queue<Integer> queue = new LinkedList<Integer>(); // 큐 생성
+		queue.add(start); // 시작 노드를 큐에 삽입
+		visited[start] = true; // 시작 노드 방문 처리
+		
+		// 큐가 비었다 = 더이상 연결된 정점이 없다는 뜻이므로
+		// 큐가 비어있지 않은 동안 반복
+		while(!queue.isEmpty()) {
+			int vertex = queue.poll(); // 현재 정점을 큐에서 꺼냄
+			sb.append(vertex + " ");
+			
+			// 현재 정점과 연결된 정점 수만큼 반복
+			for(int i = 0; i < graph[vertex].size(); i++) {
+				int next = graph[vertex].get(i); // 현재 정점과 연결된 정점
+				
+				if(!visited[next]) { // 연결된 노드에 방문한 적이 없다면
+					visited[next] = true; // 방문처리 후
+					queue.add(next); // 큐에 삽입
+				}
+			}
+		}
 	}
 }
