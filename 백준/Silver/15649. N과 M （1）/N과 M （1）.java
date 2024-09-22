894a6098b6d82c2152fb130e12d72b4da8ec9787
gomie1 <nagum0545@gmail.com>
2024-09-22 22:27:16 +0900

    [Silver III] Title: N과 M (1), Time: 1820 ms, Memory: 74980 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -3,40 +3,38 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 예제 2번에서 [1, 2]와 [2, 1]을 다른 것으로 보았으므로 순열!
 public class Main {
-    
-    static int N, M;
-    static int[] numbers; // 순열 저장 배열
-    static boolean[] isSelected; // 인덱스에 해당하는 숫자가 사용 중인지 저장하는 배열
-    
+    static int n, r, arr[];
+    static boolean[] visited;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); // 3
-        M = Integer.parseInt(st.nextToken()); // 1
-        
-        numbers = new int[M];
-        isSelected = new boolean[N+1];
-        
+
+        n = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+
+        arr = new int[r];
+        visited = new boolean[n+1];
         permutation(0);
     }
-    
+
     private static void permutation(int cnt) {
-        if(cnt == M) { // 순열 생성 완료 조건
-            for(int i = 0; i < M; i++) {
-                System.out.print(numbers[i] + " ");
+        if(cnt == r) {
+            for (int i = 0; i < r; i++) {
+                System.out.print(arr[i] + " ");
             }
             System.out.println();
             return;
         }
-        else {
-            for(int i = 1; i <= N; i++) {
-                if(!isSelected[i]) {
-                    numbers[cnt] = i;
-                    isSelected[i] = true;
-                    permutation(cnt+1);
-                    isSelected[i] = false;
-                }
+
+        for (int i = 1; i <= n; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                arr[cnt] = i;
+                permutation(cnt+1);
+                visited[i] = false;
             }
         }
     }
