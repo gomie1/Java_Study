7ab1541fcb322f435eeab5c031590c74943f67f4
gomie1 <nagum0545@gmail.com>
2024-10-11 15:49:36 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 100 ms, Memory: 14052 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -1,55 +1,46 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+	private static int N, S[], B[], res;
 
-	static int N, S[], B[], res;
-	static boolean[] isSelected;
-	
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine()); // 재료의 개수
 		
-		// 신맛과 쓴맛 입력 받기
 		StringTokenizer st;
-		S = new int[N];
-		B = new int[N];
+		S = new int[N]; // 신맛
+		B = new int[N]; // 쓴맛
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken()); 
+			int b = Integer.parseInt(st.nextToken()); 
 			
-			S[i] = Integer.parseInt(st.nextToken());
-			B[i] = Integer.parseInt(st.nextToken());
+			S[i] = s;
+			B[i] = b;
 		}
 		
-		// 재료의 개수가 1개이면 바로 결과를 출력하고 종료
-		if(N == 1) {
-			System.out.println(Math.abs(S[0] - B[0]));
-			return;
-		}
-		
-		// 재료의 개수가 2개 이상인 경우
-		isSelected = new boolean[N];
 		res = Integer.MAX_VALUE;
-		cooking(0, 1, 0);
-		
+		dfs(0, 1, 0);
 		System.out.println(res);
 	}
-
-	private static void cooking(int cnt, int s, int b) {
-		if(cnt == N) { // 종료 조건
-			if(s != 1 && b != 0) { // 1개 이상의 재료를 선택했다면
-				res = Math.min(res, Math.abs(s - b)); // 최소값 산출
-			}
+	
+	private static void dfs(int cnt, int sum_s, int sum_b) {
+		if(cnt == N) {
+			if(sum_s == 1 && sum_b == 0) return; // 아무 재료도 선택하지 않은 경우는 패스
+			
+			// 최소값 찾기
+			int value = Math.abs(sum_s - sum_b);
+			if(res > value) res = value; 
 			return;
 		}
 		
-		// 현재 재료를 선택하는 경우
-		isSelected[cnt] = true;
-		cooking(cnt+1, s*S[cnt], b+B[cnt]);
+		// 현재 재료를 선택하지 않는 경우
+		dfs(cnt+1, sum_s, sum_b);
 		
-		// 현재 재료를 선택하지 않은 경우
-		isSelected[cnt] = false;
-		cooking(cnt+1, s, b);
+		// 현재 재료를 선택하는 경우
+		dfs(cnt+1, sum_s*S[cnt], sum_b+B[cnt]);
 	}
 }
