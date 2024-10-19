5ba9a7ceb2c5aefecd90e8d18202682f76724403
gomie1 <nagum0545@gmail.com>
2024-10-20 01:11:09 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 222 ms, Memory: 30,988 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
@@ -4,41 +4,41 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
+    static int[][] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int test_case = 1; test_case <= 10; test_case++) {
+            int len = Integer.parseInt(br.readLine());
 
-        for(int test_case = 1; test_case <= 10; test_case++) {
-            int N = Integer.parseInt(br.readLine()); // 정사각형 테이블의 한 변의 길이
-
-            // 테이블의 초기 모습 입력 받기
             StringTokenizer st;
-            int[][] table = new int[N][N];
-            for(int i = 0; i < N; i++) {
+            arr = new int[100][100]; // 1: N극, 2: S극
+            for (int i = 0; i < 100; i++) {
                 st = new StringTokenizer(br.readLine());
-                for(int j = 0; j < N; j++) {
-                    table[i][j] = Integer.parseInt(st.nextToken());
+                for (int j = 0; j < 100; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            // 1 : N극 성질, 2 : S극 성질
-            int ans = 0;
-            for(int i = 0; i < 100; i++) {
-                int prev = 0;
-                for(int j = 0; j < 100; j++) {
-                    // 각 열마다 체크하기 위해 [j][i]로 설정
-                    if(table[j][i] == 1) { // 현재 위치의 값이 1이면
-                        prev = 1; // prev를 1로 표시
-                    } // 현재 위치 값이 2이면 N극 쪽으로 떨어지기 때문에 고려 X
+            int res = 0;
+            for (int j = 0; j < 100; j++) {
+                boolean flag = false;
+                for (int i = 0; i < 100; i++) {
+                    if(arr[i][j] == 2) { // 현재 자성체가 S극일 때
+                        if(!flag) continue; // N극 자성체를 만난적이 없다면 테이블 아래로 떨어짐
+                        else {
+                            res++; // N극 자성체를 만난적이 있다면 교착상태가 됨
+                            flag = false; // 새로운 교착상태가 있을 수 있으므로 flag 초기화
+                        }
+                    }
 
-                    // 현재 위치 이전에 같은 열에 1번이 있는 상태에서 2번을 만났다면 교착상태
-                    if(prev == 1 && table[j][i] == 2) {
-                        ans += 1;
-                        prev = 0;
+                    if(arr[i][j] == 1) { //현재 자성체가 N극 이라면
+                        flag = true; // N극 자성체 방문 처리
                     }
                 }
             }
 
-            System.out.println("#" + test_case + " " + ans);
+            System.out.println("#" + test_case + " " + res);
         }
     }
 }
