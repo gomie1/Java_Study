e8d2f68de4173c9c7029bab69c322c05d0295b18
gomie1 <nagum0545@gmail.com>
2024-11-11 22:58:17 +0900

    [D4] Title: 서로소 집합, Time: 2,835 ms, Memory: 132,812 KB -BaekjoonHub

---
+++ SWEA/D4/3289. 서로소 집합/README.md
@@ -0,0 +1,15 @@
+# [D4] 서로소 집합 - 3289 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWBJKA6qr2oDFAWr) 
+
+### 성능 요약
+
+메모리: 132,812 KB, 시간: 2,835 ms, 코드길이: 1,393 Bytes
+
+### 제출 일자
+
+2024-11-11 22:58
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static int n, parents[];
+	static String res;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int test_case = 1; test_case <= T; test_case++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			
+			parents = new int[n+1];
+			for(int i = 1; i <= n; i++) {
+				parents[i] = i;
+			}
+			
+			res = "";
+			for(int i = 0; i < m; i++) {
+				st = new StringTokenizer(br.readLine());
+				int num = Integer.parseInt(st.nextToken());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				
+				if(num == 0) union(a, b);
+				else {
+					if(findSet(a) == findSet(b)) res += "1";
+					else res += "0";
+				}
+			}
+			
+			System.out.println("#" + test_case + " " + res);
+		}
+	}
+	
+	private static void union(int a, int b) {
+		int aRoot = findSet(a);
+		int bRoot = findSet(b);
+		
+		if(aRoot != bRoot) {
+			parents[bRoot] = aRoot;
+		}
+	}
+	
+	private static int findSet(int a) {
+		if(parents[a] == a) return a;
+		return parents[a] = findSet(parents[a]);
+	}
+}
