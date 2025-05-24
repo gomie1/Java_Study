1ec9125960a4818c62b1b85ec24712573bd6888a
gomie1 <nagum0545@gmail.com>
2025-05-24 23:20:29 +0900

    [Silver III] Title: N과 M (1), Time: 304 ms, Memory: 33932 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,39 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
-// 예제 2번에서 [1, 2]와 [2, 1]을 다른 것으로 보았으므로 순열!
 public class Main {
-    static int n, r, arr[];
     static boolean[] visited;
+    static int n, m;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         n = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        arr = new int[r];
         visited = new boolean[n+1];
-        permutation(0);
+        permutation("", 0);
+
+        System.out.println(sb);
     }
 
-    private static void permutation(int cnt) {
-        if(cnt == r) {
-            for (int i = 0; i < r; i++) {
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
+    static void permutation(String ans, int depth) {
+        if (depth == m) {
+            sb.append(ans);
+            sb.append("\n");
             return;
         }
 
         for (int i = 1; i <= n; i++) {
-            if(!visited[i]) {
+            if (!visited[i]) {
                 visited[i] = true;
-                arr[cnt] = i;
-                permutation(cnt+1);
+                permutation(ans + Integer.toString(i) + " ", depth + 1);
                 visited[i] = false;
             }
         }
