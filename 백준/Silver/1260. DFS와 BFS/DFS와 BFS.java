ccb7e3a1b1d3f79fd6e5900d890602b539aad531
gomie1 <nagum0545@gmail.com>
2025-05-26 23:58:35 +0900

    [Silver II] Title: DFS와 BFS, Time: 212 ms, Memory: 20252 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,87 +1,69 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	static ArrayList<Integer>[] graph;
-	static boolean[] visited;
-	static StringBuilder sb = new StringBuilder();
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken()); // 정점의 개수
-		int M = Integer.parseInt(st.nextToken()); // 간선의 개수
-		int V = Integer.parseInt(st.nextToken()); // 시작 정점 번호
-		
-		// ArrayList 2차원 배열 선언
-		graph = new ArrayList[N+1];
-		for(int i = 1; i <= N; i++) {
-			graph[i] = new ArrayList<Integer>();
-		}
-		
-		// 정점들 연결 (양방향)
-		for(int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			graph[a].add(b);
-			graph[b].add(a);
-		}
-		
-		// 방문할 수 있는 정점이 여러 개인 경우 정점 번호가 작은 것부터 방문하기 위해 그래프 정렬
-		for(int i = 1; i <= N; i++) {
-			Collections.sort(graph[i]);
-		}
-		
-		visited = new boolean[N+1];
-		dfs(V);
-		sb.append("\n");
-		
-		visited = new boolean[N+1];
-		bfs(V);
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
 
-	private static void dfs(int cur) {
-		visited[cur] = true; // 현재 정점 방문처리
-		sb.append(cur + " ");
-		
-		for(int vertex : graph[cur]) { // 현재 정점과 연결된 정점들에 대해 하나씩 순회
-			if(!visited[vertex]) { // 연결된 정점에 방문한 적이 없다면
-				dfs(vertex); // 재귀를 통해 해당 정점으로 이동
-			}
-		}
-	}
-	
-	private static void bfs(int start) {
-		Queue<Integer> queue = new LinkedList<Integer>(); // 큐 생성
-		queue.add(start); // 시작 노드를 큐에 삽입
-		visited[start] = true; // 시작 노드 방문 처리
-		
-		// 큐가 비었다 = 더이상 연결된 정점이 없다는 뜻이므로
-		// 큐가 비어있지 않은 동안 반복
-		while(!queue.isEmpty()) {
-			int vertex = queue.poll(); // 현재 정점을 큐에서 꺼냄
-			sb.append(vertex + " ");
-			
-			// 현재 정점과 연결된 정점 수만큼 반복
-			for(int i = 0; i < graph[vertex].size(); i++) {
-				int next = graph[vertex].get(i); // 현재 정점과 연결된 정점
-				
-				if(!visited[next]) { // 연결된 노드에 방문한 적이 없다면
-					visited[next] = true; // 방문처리 후
-					queue.add(next); // 큐에 삽입
-				}
-			}
-		}
-	}
+        graph = new ArrayList[N+1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        visited = new boolean[N+1];
+        DFS(V);
+        sb.append("\n");
+
+        visited = new boolean[N+1];
+        BFS(V);
+        System.out.println(sb);
+    }
+
+    static void DFS(int cur) {
+        visited[cur] = true;
+        sb.append(cur).append(" ");
+
+        for (int node : graph[cur]) {
+            if (!visited[node]) DFS(node);
+        }
+    }
+
+    static void BFS(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            sb.append(cur).append(" ");
+
+            for (int node: graph[cur]) {
+                if (!visited[node]) {
+                    queue.offer(node);
+                    visited[node] = true;
+                }
+            }
+        }
+    }
 }
