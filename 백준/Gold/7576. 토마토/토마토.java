a82786c41002067395ade80442f9d2b8443b1c96
gomie1 <nagum0545@gmail.com>
2025-05-31 18:26:03 +0900

    [Gold V] Title: 토마토, Time: 552 ms, Memory: 127604 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,71 +1,57 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N, M, box[][], tCnt, cnt, res;
-	static Queue<int[]> q = new LinkedList<>();
-	static boolean[][] visited;
-	
-	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		box = new int[M][N]; // 0: 익지 않은 토마토, 1: 익은 토마토, -1: 빈 칸
-		tCnt = 0; // 총 토마토의 개수
-		cnt = 0; // 익은 토마토의 개수
-		visited = new boolean[M][N];
-		for(int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < N; j++) {
-				box[i][j] = Integer.parseInt(st.nextToken());
-				if(box[i][j] == 1) {
-					tCnt++;
-					q.offer(new int[] {i, j}); // 익은 토마토의 위치를 큐에 삽입
-					visited[i][j] = true;
-				}
-				
-				if(box[i][j] == 0) tCnt++;
-			}
-		}
-		
-		cnt = q.size();
-		res = 0;
-		bfs();
-		//System.out.println("tCnt = " + tCnt + ", cnt = " + cnt);
-		System.out.println(cnt == tCnt ? res-1 : -1);
-	}
-	
-	private static void bfs() {
-		while(!q.isEmpty()) {
-			int size = q.size();
-			for(int i = 0; i < size; i++) {
-				int[] cur = q.poll();
-				
-				
-				for(int j = 0; j < 4; j++) {
-					int nx = cur[0] + dx[j];
-					int ny = cur[1] + dy[j];
-					
-					if(nx < 0 || nx >= M || ny < 0 || ny >= N || visited[nx][ny] || box[nx][ny] == -1 || box[nx][ny] == 1) continue;
-					
-					//System.out.println("nx = " + nx + ", ny = " + ny);
-					visited[nx][ny] = true;
-					cnt++;
-					q.offer(new int[] {nx, ny});
-					
-				}
-			}
-			
-			res++;
-		}
-	}
+    static int N, M, mtrx[][], cnt, ans;
+    static Queue<int[]> queue;
+
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        mtrx = new int[N][M];
+        queue = new LinkedList<>();
+        cnt = 0; // 익지 않은 토마토의 개수
+        ans = 0; // 날짜
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                mtrx[i][j] = Integer.parseInt(st.nextToken());
+                if (mtrx[i][j] == 1) queue.offer(new int[] {i, j, 0});
+                else if (mtrx[i][j] == 0) cnt++;
+            }
+        }
+
+        if (cnt == 0) {
+            System.out.println(ans);
+            return;
+        }
+
+        bfs();
+        if (cnt == 0) System.out.println(ans);
+        else System.out.println(-1);
+    }
+
+    static void bfs() {
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            if (ans < cur[2]) ans = cur[2];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M || mtrx[nx][ny] != 0) continue;
+
+                queue.offer(new int[] {nx, ny, cur[2] + 1});
+                mtrx[nx][ny] = 1;
+                cnt--;
+            }
+        }
+    }
 }
