de4451da78e6be594b683910de7265a4ecec47c9
gomie1 <nagum0545@gmail.com>
2025-06-05 21:12:02 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,34 +1,43 @@
 import java.util.*;
 
 class Solution {
-    static boolean[] visited;
+    static boolean[][] visited;
+    
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
     
     public int solution(int n, int[][] computers) {
         int answer = 0;
+        visited = new boolean[n][n];
         
-        visited = new boolean[n];
-        for(int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                bfs(i, n, computers);
-                answer++;
+        for (int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if (!visited[i][j] && computers[i][j] == 1) {
+                    bfs(i, j, n, computers);
+                    answer++;
+                }
             }
         }
+        
         return answer;
     }
     
-    void bfs(int node, int len, int[][] arr) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(node);
-        visited[node] = true;
+    static void bfs (int start_x, int start_y, int n, int[][] computers) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] {start_x, start_y});
+        visited[start_x][start_y] = true;
         
         while (!q.isEmpty()) {
-            int cur = q.poll();
+            int[] cur = q.poll();
             
-            for(int nxt = 0; nxt < len; nxt++) {
-                if (!visited[nxt] && arr[cur][nxt] == 1) {
-                    q.offer(nxt);
-                    visited[nxt] = true;
-                }
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+                
+                if (nx < 0 || nx >= n || ny < 0 || ny >= n || visited[nx][ny] || computers[nx][ny] == 0) continue;
+                
+                visited[nx][ny] = true;
+                q.offer(new int[] {nx, ny});
             }
         }
     }
