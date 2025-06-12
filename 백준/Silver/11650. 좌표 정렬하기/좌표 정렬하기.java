90210edafac5e7476b03d93f9d3d4d0197af849f
gomie1 <nagum0545@gmail.com>
2025-06-13 01:28:35 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 636 ms, Memory: 50784 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -7,19 +7,19 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
 
         StringTokenizer st;
-        List<int[]> list = new ArrayList<>();
+        int[][] arr = new int[N][2];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            list.add(new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+            arr[i] = new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
         }
 
-        list.sort((a, b) -> {
+        Arrays.sort(arr, (a, b) -> {
             if (a[0] == b[0]) return a[1] - b[1];
             return a[0] - b[0];
         });
 
         StringBuilder sb = new StringBuilder();
-        for (int[] cur : list) {
+        for (int[] cur : arr) {
             sb.append(cur[0]).append(" ").append(cur[1]).append('\n');
         }
 
