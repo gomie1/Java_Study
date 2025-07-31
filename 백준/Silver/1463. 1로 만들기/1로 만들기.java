144d67bc3e051a1ee9f952f8044df9ba52ba4460
gomie1 <nagum0545@gmail.com>
2025-07-31 12:45:13 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18176 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N+1];
+        dp[1] = 0; // 1에는 연산을 하지 않아도 1을 만들 수 있으므로 연산 0번
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1;
+
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        }
+
+        System.out.println(dp[N]);
+    }
+}
