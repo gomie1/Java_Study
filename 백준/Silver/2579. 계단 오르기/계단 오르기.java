efef2c2f847791b254101309c6c3f1196e8f5cca
gomie1 <nagum0545@gmail.com>
2025-08-02 17:59:33 +0900

    [Silver III] Title: 계단 오르기, Time: 108 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] score = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            score[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[Math.max(N+1, 4)];
+        dp[1] = score[1];
+        if (N >= 2) dp[2] = score[1] + score[2];
+        if (N >= 3) dp[3] = Math.max(score[1] + score[3], score[2] + score[3]);
+
+        for (int i = 4; i <= N; i++) {
+            dp[i] = Math.max(dp[i-2], dp[i-3] + score[i-1]) + score[i];
+        }
+        
+        System.out.println(dp[N]);
+    }
+}
