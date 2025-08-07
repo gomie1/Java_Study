a872671b84313b9918fe74acac2b059924cff7ce
gomie1 <nagum0545@gmail.com>
2025-08-07 22:48:33 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 588 ms, Memory: 142084 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); // 정점의 개수
+        int M = Integer.parseInt(st.nextToken()); // 간선의 개수
+
+        graph = new ArrayList[N+1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        int cnt = 0;
+        visited = new boolean[N+1];
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                bfs(i);
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+    static void bfs(int node) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(node);
+        visited[node] = true;
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+
+            for (int nxt : graph[cur]) {
+                if (!visited[nxt]) {
+                    visited[nxt] = true;
+                    q.offer(nxt);
+                }
+            }
+        }
+    }
+}
