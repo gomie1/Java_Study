3b2355df4906fd601ecefabd6e485df40da535db
gomie1 <nagum0545@gmail.com>
2025-08-09 16:00:46 +0900

    [Silver II] Title: 최대 힙, Time: 272 ms, Memory: 26376 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x == 0) sb.append(pq.isEmpty() ? 0 : pq.poll()).append('\n');
+            else pq.offer(x);
+        }
+
+        System.out.print(sb);
+    }
+}
