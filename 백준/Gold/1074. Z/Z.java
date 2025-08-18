c824fcc4509c63044d591d36655394d90d8b69a2
gomie1 <nagum0545@gmail.com>
2025-08-18 15:54:50 +0900

    [Gold V] Title: Z, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -1,48 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N, r, c, arr[][], res;
-	static int[][] Z = {{0, 1}, {1, 0}, {1, 1}};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		r = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		
-		res = 0;
-		dfs(0, 0, (int) Math.pow(2, N));		
-	}
+        int ans = 0;
+        for (int n = N; n > 0; n--) {
+            int half = 1 << (n-1); // 2^(n-1)
+            int num = 0;
 
-	// Point: (r, c)가 몇 사분면에 속해있는지 구하고, 그 부분만 재귀를 돌리는 것!!
-	private static void dfs(int x, int y, int size) {
-		if(size == 1) {
-			System.out.println(res);
-			return;
-		}
-		
-		int half = size / 2;
-		if(r < x+half && c < y+half) { // 1 사분면
-			dfs(x, y, half);
-		}
-		
-		if(r < x+half && y+half <= c) { // 2 사분면
-			res += half*half;
-			dfs(x, y+half, half);
-		}
-		
-		if(x+half <= r && c < y+half) { // 3 사분면
-			res += half*half*2;
-			dfs(x+half, y, half);
-		}
-		
-		if(x+half <= r && y+half <= c){ // 4 사분면
-			res += half*half*3;
-			dfs(x+half, y+half, half);
-		}
-	}
+            if (r >= half) {
+                num += 2;
+                r -= half;
+            }
+
+            if (c >= half) {
+                num += 1;
+                c -= half;
+            }
+
+            ans += num * half * half;
+        }
+
+        System.out.println(ans);
+    }
 }
