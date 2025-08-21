ff8ea671e63230b2524e90d5a1d0bd961fd2dcbe
gomie1 <nagum0545@gmail.com>
2025-08-21 15:00:11 +0900

    [Gold IV] Title: 테트로미노, Time: 624 ms, Memory: 38784 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -1,82 +1,72 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	static int N, M, res=Integer.MIN_VALUE;
-	static int[][] paper;
-	static boolean[][] visited;
-	static int[] dx = {0, 1, 0, -1};
-	static int[] dy = {1, 0, -1, 0};
+    static int N, M, arr[][], ans;
+    static boolean[][] visited;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken()); // 세로 크기
-		M = Integer.parseInt(st.nextToken()); // 가로 크기
-		
-		// 종이에 값 입력 받기
-		paper = new int[N][M];
-		for(int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < M; j++) {
-				paper[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
-		visited = new boolean[N][M];
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < M; j++) {
-				visited[i][j] = true;
-				dfs(i, j, 1, paper[i][j]);
-				visited[i][j] = false;
-				others(i, j);
-			}
-		}
-		System.out.println(res);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-	private static void dfs(int x, int y, int cnt, int sum) {
-		// 상, 하, 좌, 우로 움직여서 4칸을 만들었기 때문에 무조건 블록!
-		if(cnt == 4) {
-			//System.out.println("여기 드렁옴" + sum);
-			res = Math.max(res, sum); // 최대값 찾기
-			return;
-		}
-		
-		for(int dir = 0; dir < 4; dir++) {
-			int nx = x + dx[dir];
-			int ny = y + dy[dir];
-			
-			if(nx >= N || ny >= M || nx < 0 || ny < 0) continue;
-			
-			if(!visited[nx][ny]) {
-				visited[nx][ny] = true;
-				dfs(nx, ny, cnt+1, sum+paper[nx][ny]);
-				visited[nx][ny] = false;
-			}
-		}
-	}
-	
-	// 범위 똑바로 하기
-	private static void others(int x, int y) {
-		if(x+1 < N && y-1 >= 0 && y+1 < M){
-            res = Math.max(res, paper[x][y] + paper[x+1][y] + paper[x][y-1] + paper[x][y+1]);
+        arr = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
-		
-        if(x+1 < N && x-1 >= 0 && y+1 < M){
-            res = Math.max(res, paper[x][y] + paper[x+1][y] + paper[x-1][y] + paper[x][y+1]);
+
+        visited = new boolean[N][M];
+        ans = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                visited[i][j] = true;
+                dfs(i, j, 1, arr[i][j]);
+                visited[i][j] = false;
+            }
         }
-        
-        if(x+1 < N && x-1 >= 0 && y-1 >= 0){
-            res = Math.max(res, paper[x][y] + paper[x+1][y] + paper[x-1][y] + paper[x][y-1]);
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                // 'ㅗ' 모양
+                if (i-1 >= 0 && j-1 >= 0 && j+1 < M) ans = Math.max(ans, arr[i][j] + arr[i][j-1] + arr[i][j+1] + arr[i-1][j]);
+
+                // 'ㅜ' 모양
+                if (i+1 < N && j-1 >= 0 && j+1 < M) ans = Math.max(ans, arr[i][j] + arr[i][j-1] + arr[i][j+1] + arr[i+1][j]);
+
+                // 'ㅏ' 모양
+                if (i-1 >= 0 && i+1 < N && j+1 < M) ans = Math.max(ans, arr[i][j] + arr[i-1][j] + arr[i+1][j] + arr[i][j+1]);
+
+                // 'ㅓ' 모양'
+                if (i-1 >= 0 && i+1 < N && j-1 >= 0) ans = Math.max(ans, arr[i][j] + arr[i-1][j] + arr[i+1][j] + arr[i][j-1]);
+            }
         }
-        
-        if(x-1 >= 0 && y-1 >= 0 && y+1 < M){
-            res = Math.max(res, paper[x][y] + paper[x-1][y] + paper[x][y-1] + paper[x][y+1]);
+
+        System.out.println(ans);
+    }
+
+    static void dfs(int x, int y, int cnt, int sum) {
+        if (cnt > 4) return;
+        if (cnt == 4) {
+            ans = Math.max(ans, sum);
+            return;
         }
-	}
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= N || ny < 0 || ny >= M || visited[nx][ny]) continue;
+
+            visited[nx][ny] = true;
+            dfs(nx, ny, cnt+1, sum+arr[nx][ny]);
+            visited[nx][ny] = false;
+        }
+    }
 }
