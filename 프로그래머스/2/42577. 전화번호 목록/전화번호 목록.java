b16607a4b5a46bea279042469fa7b5c8b18bb76c
gomie1 <nagum0545@gmail.com>
2025-08-26 17:00:49 +0900

    [level 2] Title: 전화번호 목록, Time: 368.93 ms, Memory: 98.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        boolean answer = true;
+        
+        for (int i = 1; i < phone_book.length; i++) {
+            if (phone_book[i].startsWith(phone_book[i-1])) {
+                answer = false;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
