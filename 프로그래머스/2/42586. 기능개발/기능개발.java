8525821881e66254e426d8146599a92b5c853bd6
gomie1 <nagum0545@gmail.com>
2025-09-07 15:02:48 +0900

    [level 2] Title: 기능개발, Time: 3.98 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,36 +3,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int len = progresses.length;
-        int time = 0;
-        int cnt = 1;
-        ArrayList<Integer> res = new ArrayList<>();
-        
-        for (int i = 0; i < len; i++) {
-            int val = 100 - progresses[i];
-            
-            if (val % speeds[i] == 0) val /= speeds[i];
-            else val = (val / speeds[i]) + 1;
-            
-            if (i == 0) {
-                time = val;
-                continue;
+        int[] dayOfEnd = new int[100]; // 날짜별 배포 개수를 기록할 배열
+        int day = 0;
+        for(int i = 0; i < progresses.length; i++) {
+            // 기능 i가 끝나는 날짜를 구함
+            while(progresses[i] + (day * speeds[i]) < 100) {
+                day++;
             }
             
-            if (val <= time) cnt++;
-            else {
-                res.add(cnt);
-                cnt = 1;
-                time = val;
-            }
+            // i번째 기능은 'day'날짜에 완료되므로 해당 날짜 카운트 증가
+            dayOfEnd[day]++;
         }
-        res.add(cnt);
         
-        int size = res.size();
-        int[] answer = new int[size];
-        for (int i = 0; i < size; i++) {
-            answer[i] = res.get(i);
-        }
-        return answer;
+        // 0이 아닌 값만 추출해서 결과 배열로 반환
+        return Arrays.stream(dayOfEnd).filter(i -> i != 0).toArray();
     }
 }
