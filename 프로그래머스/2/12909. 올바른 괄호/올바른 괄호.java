141fe401f70d1a3c7bba446edcc4b50e2e9b59d2
gomie1 <nagum0545@gmail.com>
2025-09-09 14:48:30 +0900

    [level 2] Title: 올바른 괄호, Time: 7.31 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,16 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int cnt = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') cnt++;
+            else {
+                if (cnt == 0) return false;
+                else cnt--;
+            }
+        }
+        
+        if (cnt != 0) return false;
+        return answer;
+    }
+}
