6bd79255bbf41f70d82a4ad8cebd62be4644a6d6
Nageum Jeong <nagum0545@gmail.com>
2025-10-23 11:34:18 +0900

    [Silver II] Title: 한 줄로 서기, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -7,37 +7,28 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
-            if (o1[0] != o2[0]) return o1[0] - o2[0];
-            return o2[1] - o1[1];
-        });
-        for (int i = 1; i <= N; i++) {
-            pq.offer(new int[] {Integer.parseInt(st.nextToken()), i});
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        List<int[]> list = new LinkedList<>();
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-
-            if (list.isEmpty()) list.add(cur);
-            else {
-                int cnt = 0;
-                for (int i = 0; i <= list.size(); i++) {
-                    if (cnt == cur[0]) {
-                        list.add(i, cur);
+        int[] line = new int[N];
+        for (int i = 0; i < N; i++) {
+            int cur = i + 1;
+            int cnt = 0;
+            for (int j = 0; j < N; j++) {
+                if (line[j] == 0) {
+                    if (cnt == arr[i]) {
+                        line[j] = cur;
                         break;
                     }
-
-                    if (cnt < cur[0] && cur[1] < list.get(i)[1]) cnt++;
+                    cnt++;
                 }
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            sb.append(list.get(i)[1]).append(" ");
-        }
-
+        for (int n : line) sb.append(n).append(" ");
         System.out.println(sb);
     }
 }
