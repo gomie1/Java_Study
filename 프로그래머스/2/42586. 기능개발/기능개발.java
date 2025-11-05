27c3096bd8972bf25664a6de59074b34ec1edb81
Nageum Jeong <nagum0545@gmail.com>
2025-11-05 18:00:51 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,21 +1,35 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] dayOfEnd = new int[100]; // 날짜별 배포 개수를 기록할 배열
-        int day = 0;
-        for(int i = 0; i < progresses.length; i++) {
-            // 기능 i가 끝나는 날짜를 구함
-            while(progresses[i] + (day * speeds[i]) < 100) {
-                day++;
-            }
-            
-            // i번째 기능은 'day'날짜에 완료되므로 해당 날짜 카운트 증가
-            dayOfEnd[day]++;
+        // 1. 각 작업별 완성하기까지 걸리는 시간 계산하기
+        int[] days = new int[progresses.length];
+        for (int i = 0; i < progresses.length; i++) {
+            if ((100 - progresses[i]) % speeds[i] == 0) days[i] = (100 - progresses[i]) / speeds[i];
+            else days[i] = ((100 - progresses[i]) / speeds[i]) + 1;
         }
         
-        // 0이 아닌 값만 추출해서 결과 배열로 반환
-        return Arrays.stream(dayOfEnd).filter(i -> i != 0).toArray();
+        int idx = 0;
+        int maxDay = days[idx++];
+        int cnt = 1;
+        List<Integer> cntList = new ArrayList<>();
+        while (idx < days.length) {
+            if (days[idx] <= maxDay) cnt++;
+            else {
+                cntList.add(cnt);
+                cnt = 1;
+                maxDay = days[idx];
+            }
+            
+            idx++;
+        }
+        cntList.add(cnt);
+        
+        int[] ans = new int[cntList.size()];
+        for (int i = 0; i < cntList.size(); i++) {
+            ans[i] = cntList.get(i);
+        }
+        
+        return ans;
     }
 }
