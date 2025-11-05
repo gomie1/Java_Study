b07b739342b4c732ba1ffa545cf4b6fe4ad45123
Nageum Jeong <nagum0545@gmail.com>
2025-11-05 17:29:42 +0900

    [Silver III] Title: 피보나치 함수, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,27 +1,25 @@
 import java.io.*;
 
-public class Main  {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
-        StringBuilder sb = new StringBuilder();
-        int[][] cnt = new int[41][2];
-        cnt[0][0] = 1;
-        cnt[0][1] = 0;
-        cnt[1][0] = 0;
-        cnt[1][1] = 1;
+        int[][] fibonacci = new int[41][2]; // [zero, one]
+        fibonacci[0][0] = 1; // n이 0인 경우
+        fibonacci[1][1] = 1; // n이 1인 경우
 
         for (int i = 2; i <= 40; i++) {
-            cnt[i][0] = cnt[i-1][0] + cnt[i-2][0];
-            cnt[i][1] = cnt[i-1][1] + cnt[i-2][1];
+            fibonacci[i][0] = fibonacci[i-1][0] + fibonacci[i-2][0];
+            fibonacci[i][1] = fibonacci[i-1][1] + fibonacci[i-2][1];
         }
 
-        for (int i = 0; i < T; i++) {
+        StringBuilder sb = new StringBuilder();
+        for (int test_case = 0; test_case < T; test_case++) {
             int N = Integer.parseInt(br.readLine());
-            sb.append(cnt[N][0]).append(" ").append(cnt[N][1]).append('\n');
+            sb.append(fibonacci[N][0]).append(" ").append(fibonacci[N][1]).append('\n');
         }
 
-        System.out.print(sb);
+        System.out.println(sb);
     }
 }
