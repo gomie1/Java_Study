194a89e3c300562cfcb7df246d33e6c7b4d2cada
Nageum Jeong <nagum0545@gmail.com>
2025-11-06 16:37:48 +0900

    [level 2] Title: 프로세스, Time: 1.08 ms, Memory: 92.7 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,22 +2,23 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        ArrayDeque<int[]> dq = new ArrayDeque<>();
+        Deque<int[]> dq = new ArrayDeque<>();
         for (int i = 0; i < priorities.length; i++) {
-            dq.add(new int[] {i, priorities[i]});
+            dq.add(new int[] {priorities[i], i});
         }
+        
         Arrays.sort(priorities);
-        int idx = priorities.length - 1;
         
         int answer = 0;
+        int idx = priorities.length - 1;
         while (!dq.isEmpty()) {
             int[] cur = dq.poll();
             
-            if (cur[1] < priorities[idx]) dq.addLast(new int[] {cur[0], cur[1]});
+            if (cur[0] < priorities[idx]) dq.add(new int[] {cur[0], cur[1]});
             else {
                 answer++;
                 idx--;
-                if (cur[0] == location) break;
+                if (cur[1] == location) break;
             }
         }
         
