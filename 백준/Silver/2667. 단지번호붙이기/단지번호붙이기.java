5b9e31f5b771cd4910b98ed621017c16ea66d4d6
Nageum Jeong <nagum0545@gmail.com>
2025-11-07 14:18:43 +0900

    [Silver I] Title: 단지번호붙이기, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,61 +2,67 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int[][] map;
+    static boolean[][] visited;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int[][] map = new int[N][N];
+        map = new int[N][N];
         for (int i = 0; i < N; i++) {
-            char[] input = br.readLine().toCharArray();
+            String input = br.readLine();
             for (int j = 0; j < N; j++) {
-                map[i][j] = input[j] - '0';
+                map[i][j] = input.charAt(j) - '0';
             }
         }
 
-        ArrayList<Integer> ans = new ArrayList<>();
-        int total = 0;
-        Queue<int[]> q = new LinkedList<>();
-        boolean[][] visited = new boolean[N][N];
-
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-
+        StringBuilder sb = new StringBuilder();
+        visited = new boolean[N][N];
+        int ans = 0;
+        List<Integer> home = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (!visited[i][j] && map[i][j] == 1) {
-                    q.offer(new int[] {i, j});
-                    visited[i][j] = true;
-                    int cnt = 0;
-                    total++;
-
-                    while (!q.isEmpty()) {
-                        int[] cur = q.poll();
-                        cnt++;
-
-                        for (int k = 0; k < 4; k++) {
-                            int nx = cur[0] + dx[k];
-                            int ny = cur[1] + dy[k];
-
-                            if (nx < 0 || nx >= N || ny < 0 || ny >= N ||
-                                    visited[nx][ny] || map[nx][ny] == 0) continue;
-
-                            q.offer(new int[] {nx, ny});
-                            visited[nx][ny] = true;
-                        }
-                    }
-
-                    ans.add(cnt);
+                    home.add(homeCnt(i, j, N));
+                    ans++;
                 }
             }
         }
 
-        Collections.sort(ans);
+        Collections.sort(home);
+        sb.append(ans).append('\n');
+        for (int i = 0; i < home.size(); i++) {
+            sb.append(home.get(i)).append('\n');
+        }
 
-        StringBuilder sb = new StringBuilder();
-        sb.append(total).append('\n');
-        for (int v : ans) sb.append(v).append('\n');
+        System.out.println(sb);
+    }
 
-        System.out.print(sb);
+    static int homeCnt(int sx, int sy, int n) {
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        Queue<int[]> q = new LinkedList<>();
+        visited[sx][sy] = true;
+        q.offer(new int[] {sx, sy});
+
+        int cnt = 0;
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            cnt++;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= n || visited[nx][ny] || map[nx][ny] == 0) continue;
+
+                q.offer(new int[] {nx, ny});
+                visited[nx][ny] = true;
+            }
+        }
+
+        return cnt;
     }
 }
