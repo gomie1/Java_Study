5ca2092b259c69ee5f5b4db2401aaa193af7bccc
Nageum Jeong <nagum0545@gmail.com>
2025-11-12 16:59:09 +0900

    [level 3] Title: 네트워크, Time: 0.44 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 84.1 MB, 시간: 0.28 ms
+메모리: 76.5 MB, 시간: 0.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 05일 21:12:00
+2025년 11월 12일 16:59:07
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,44 +1,30 @@
 import java.util.*;
 
 class Solution {
-    static boolean[][] visited;
-    
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        visited = new boolean[n][n];
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[n];
         
+        int answer = 0;
         for (int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if (!visited[i][j] && computers[i][j] == 1) {
-                    bfs(i, j, n, computers);
-                    answer++;
+            if (!visited[i]) {
+                visited[i] = true;
+                q.add(i);
+                answer++;
+                
+                while (!q.isEmpty()) {
+                    int cur = q.poll(); // 현재 컴퓨터 번호
+                    
+                    for (int nxt = 0; nxt < n; nxt++) {
+                        if (nxt == cur || visited[nxt] || computers[cur][nxt] == 0) continue;
+                        
+                        visited[nxt] = true;
+                        q.add(nxt);
+                    }
                 }
             }
         }
         
         return answer;
-    }
-    
-    static void bfs (int start_x, int start_y, int n, int[][] computers) {
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {start_x, start_y});
-        visited[start_x][start_y] = true;
-        
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            
-            for (int i = 0; i < 4; i++) {
-                int nx = cur[0] + dx[i];
-                int ny = cur[1] + dy[i];
-                
-                if (nx < 0 || nx >= n || ny < 0 || ny >= n || visited[nx][ny] || computers[nx][ny] == 0) continue;
-                
-                visited[nx][ny] = true;
-                q.offer(new int[] {nx, ny});
-            }
-        }
     }
 }
