2b9ea1db24540483b7072df383dfb97f61ce4800
Nageum Jeong <nagum0545@gmail.com>
2025-11-15 20:36:58 +0900

    [Gold IV] Title: 최단경로, Time: 648 ms, Memory: 108064 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,80 +1,56 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int V, E, K, dist[];
-	static ArrayList<Edge>[] graph;
-	
-	static class Edge implements Comparable<Edge> {
-		int num;
-		int weight;
-		
-		public Edge(int num, int weight) {
-			super();
-			this.num = num;
-			this.weight = weight;
-		}
+    static final int INF = 1_000_000_000;
 
-		@Override
-		public int compareTo(Edge o) {
-			return this.weight - o.weight;
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		V = Integer.parseInt(st.nextToken()); // 정점의 개수
-		E = Integer.parseInt(st.nextToken()); // 간선의 개수
-		K = Integer.parseInt(br.readLine()); // 시장 정점의 번호
-		
-		graph = new ArrayList[V+1];
-		for(int i = 1; i <= V; i++) {
-			graph[i] = new ArrayList<>();
-		}
-		
-		for(int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			
-			graph[u].add(new Edge(v, w));
-		}
-		
-		dist = new int[V+1];
-		Arrays.fill(dist, Integer.MAX_VALUE);
-		dist[K] = 0;
-		
-		Dijkstra();
-		for(int i = 1; i <= V; i++) {
-			System.out.println(dist[i] != Integer.MAX_VALUE ? dist[i] : "INF");
-		}
-	}
+        ArrayList<int[]>[] graph = new ArrayList[V+1];
+        for (int i = 1; i <= V; i++) {
+            graph[i] = new ArrayList<>();
+        }
 
-	private static void Dijkstra() {
-		boolean[] visited = new boolean[V+1];
-		PriorityQueue<Edge> pq = new PriorityQueue<>();
-		pq.offer(new Edge(K, 0));
-		
-		while(!pq.isEmpty()) {
-			Edge cur = pq.poll();
-			
-			if(visited[cur.num]) continue;
-			visited[cur.num] = true;
-			
-			for(Edge nxt : graph[cur.num]) {
-				if(dist[nxt.num] > dist[cur.num] + nxt.weight) {
-					dist[nxt.num] = dist[cur.num] + nxt.weight;
-					pq.offer(new Edge(nxt.num, dist[nxt.num])); 
-					// 갱신된 거리를 넣어주는 이유: 넣어주지 않으면 nxt.num의 weight는 계속 초기 가중치가 되기 때문
-				}
-			}
-		}
-	}
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            graph[u].add(new int[] {v, w});
+        }
+
+        int[] dist = new int[V+1];
+        Arrays.fill(dist, INF);
+        dist[K] = 0;
+
+        // Dijkstra
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        pq.offer(new int[] {K, 0});
+
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll(); // [v, w]
+            if (dist[cur[0]] < cur[1]) continue; // 더 가까운 거리가 있다면 pass
+
+            for (int[] nxt : graph[cur[0]]) {
+                if (cur[1] + nxt[1] < dist[nxt[0]]) {
+                    dist[nxt[0]] = cur[1] + nxt[1];
+                    pq.offer(new int[] {nxt[0], dist[nxt[0]]});
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= V; i++) {
+            if (dist[i] == INF) sb.append("INF").append('\n');
+            else sb.append(dist[i]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
 }
