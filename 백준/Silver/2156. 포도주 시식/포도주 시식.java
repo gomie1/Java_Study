2de6ce7f1ff1a84a695b85fa5f7e79d204d31e5b
Nageum Jeong <nagum0545@gmail.com>
2025-11-20 13:34:49 +0900

    [Silver I] Title: 포도주 시식, Time: 120 ms, Memory: 14832 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -27,17 +27,13 @@ public class Main {
                 Math.max(dp[1], wine[0] + wine[2]), // 2번째 와인을 마시지 않는 경우 vs 0번째, 2번째 와인을 마시는 경우
                 wine[1] + wine[2] // vs 1번째, 2번째 와인을 마시는 경우
         );
-
-        int max = Math.max(dp[1], dp[2]);
         for (int i = 3; i < n; i++) {
             dp[i] = Math.max(
                     Math.max(dp[i-1], dp[i-2] + wine[i]),
                     dp[i-3] + wine[i-1] + wine[i]
             );
-
-            max = Math.max(max, dp[i]);
         }
 
-        System.out.println(max);
+        System.out.println(dp[n-1]);
     }
 }
