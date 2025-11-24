b75d05630872d375ebee44826a09581db4157d56
Nageum Jeong <nagum0545@gmail.com>
2025-11-24 17:00:28 +0900

    [Gold V] Title: 합분해, Time: 100 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static final int MOD = 1_000_000_000;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[N+1][K+1];
+        Arrays.fill(dp[0], 1);
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= K; j++) {
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % MOD;
+            }
+        }
+
+        System.out.println(dp[N][K]);
+    }
+}
