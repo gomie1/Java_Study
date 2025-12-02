73a74645b67cb34567548f394c7e1821a1106157
Nageum Jeong <nagum0545@gmail.com>
2025-12-02 12:34:53 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18084 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,15 +6,13 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
 
         int[] dp = new int[N+1];
-        dp[1] = 0; // 1에는 연산을 하지 않아도 1을 만들 수 있으므로 연산 0번
-
+        dp[1] = 0;
         for (int i = 2; i <= N; i++) {
-            dp[i] = dp[i - 1] + 1;
-
-            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            dp[i] = Math.min(i, dp[i-1] + 1);
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i/2] + 1);
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i/3] + 1);
         }
-
+        
         System.out.println(dp[N]);
     }
 }
