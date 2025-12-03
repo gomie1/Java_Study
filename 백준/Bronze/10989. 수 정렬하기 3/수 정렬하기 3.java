3e94b88e77cdb0c3ee008a751623591e2f4dff0d
Nageum Jeong <nagum0545@gmail.com>
2025-12-03 18:02:49 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1560 ms, Memory: 336824 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,18 +1,17 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[10000001];
+        int[] arr = new int[10001];
         for (int i = 0; i < N; i++) {
             arr[Integer.parseInt(br.readLine())]++;
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i <= 10000000; i++) {
+        for (int i = 0; i <= 10000; i++) {
             while (arr[i] > 0) {
                 sb.append(i).append('\n');
                 arr[i]--;
