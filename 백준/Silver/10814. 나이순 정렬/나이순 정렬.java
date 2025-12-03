39edb48d0355ca5f6efc5c7a7e71e860a371689b
Nageum Jeong <nagum0545@gmail.com>
2025-12-03 17:28:59 +0900

    [Silver V] Title: 나이순 정렬, Time: 544 ms, Memory: 45348 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -2,41 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class User implements Comparable<User> {
-        int age;
-        String name;
-        int date;
-
-        User (int age, String name, int date) {
-            this.age = age;
-            this.name = name;
-            this.date = date;
-        }
-
-        @Override
-        public int compareTo(User o) {
-            if (this.age == o.age) return this.date - o.date;
-            return this.age - o.age;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        User[] arr = new User[N];
+        int[][] info = new int[N][2]; // 순서, 나이
+        String[] name = new String[N]; // 이름
         StringTokenizer st;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            arr[i] = new User(Integer.parseInt(st.nextToken()), st.nextToken(), i);
+            info[i][0] = i;
+            info[i][1] = Integer.parseInt(st.nextToken());
+            name[i] = st.nextToken();
         }
 
-        Arrays.sort(arr);
-        StringBuilder sb = new StringBuilder();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
+            if (o1[1] != o2[1]) return o1[1] - o2[1];
+            return o1[0] - o2[0];
+        });
+
         for (int i = 0; i < N; i++) {
-            sb.append(arr[i].age).append(" ").append(arr[i].name).append('\n');
+            pq.offer(info[i]);
         }
 
-        System.out.print(sb);
+        StringBuilder sb = new StringBuilder();
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            sb.append(cur[1]).append(" ").append(name[cur[0]]).append('\n');
+        }
+
+        System.out.println(sb);
     }
 }
