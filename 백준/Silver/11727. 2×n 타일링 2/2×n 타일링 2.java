4335b7b37419f157752ce76798ed0eafa5918b09
Nageum Jeong <nagum0545@gmail.com>
2025-12-04 22:50:48 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 104 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -1,18 +1,24 @@
 import java.io.*;
 
 public class Main {
+    static final int MOD = 10_007;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[Math.max(n+1, 3)];
-        dp[1] = 1;
-        dp[2] = 3;
-
-        for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i-1] + dp[i-2]*2) % 10007;
+        if (N == 1) {
+            System.out.println(1);
+            return;
         }
 
-        System.out.println(dp[n]);
+        int[] dp = new int[N+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            dp[i] = (dp[i-1] + dp[i-2] * 2) % MOD;
+        }
+
+        System.out.println(dp[N] % MOD);
     }
 }
