989745b6108d5d25475b718c4d5274b177506c6b
Nageum Jeong <nagum0545@gmail.com>
2025-12-05 14:08:35 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -5,19 +5,20 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
+        int[] dp = new int[12];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < 12; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < T; i++) {
             int n = Integer.parseInt(br.readLine());
-
-            int[] dp = new int[Math.max(n+1, 4)];
-            dp[1] = 1;
-            dp[2] = 2;
-            dp[3] = 4;
-
-            for (int j = 4; j <= n; j++) dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
             sb.append(dp[n]).append('\n');
         }
 
         System.out.println(sb);
-    }
+     }
 }
