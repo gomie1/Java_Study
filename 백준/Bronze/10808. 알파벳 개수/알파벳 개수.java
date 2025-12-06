d8e6f097af27dbd069e3a55120607b82d13e9f31
Nageum Jeong <nagum0545@gmail.com>
2025-12-06 23:40:05 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 100 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+
+        int[] alpha = new int[26];
+        for (char c : S.toCharArray()) {
+            int idx = (c - '0') - 49;
+            alpha[idx]++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            sb.append(alpha[i]).append(" ");
+        }
+
+        System.out.println(sb);
+     }
+}
