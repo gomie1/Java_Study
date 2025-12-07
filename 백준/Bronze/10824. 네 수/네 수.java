8940d516acf2aa0f161c089da2643ae581fd0082
Nageum Jeong <nagum0545@gmail.com>
2025-12-07 12:51:59 +0900

    [Bronze III] Title: 네 수, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] nums = br.readLine().split(" ");
+
+        System.out.println(Long.parseLong(nums[0] + nums[1]) + Long.parseLong(nums[2] + nums[3]));
+    }
+}
