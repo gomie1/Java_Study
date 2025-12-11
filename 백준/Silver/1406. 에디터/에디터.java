a4571a67c2411b29c8ed292d6cbd7c06583de4de
Nageum Jeong <nagum0545@gmail.com>
2025-12-11 17:29:34 +0900

    [Silver II] Title: 에디터, Time: 712 ms, Memory: 175944 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -5,34 +5,25 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
-        int M = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        Stack<Character> leftStack = new Stack<>();
-        for (int i = 0; i < str.length(); i++) {
-            leftStack.add(str.charAt(i));
+        Stack<Character> left = new Stack<>();
+        Stack<Character> right = new Stack<>();
+        for (char c : str.toCharArray()) {
+            left.add(c);
         }
 
-        Stack<Character> rightStack = new Stack<>();
-        for (int i = 0; i < M; i++) {
+        for (int i = 0; i < n; i++) {
             String[] cmd = br.readLine().split(" ");
-            
-            // L: 커서를 왼쪽으로 한 칸 옮김
-            if (cmd[0].equals("L") && !leftStack.isEmpty()) rightStack.push(leftStack.pop());
-            
-            // D: 커서를 오른쪽으로 한 칸 옮김
-            else if (cmd[0].equals("D") && !rightStack.isEmpty()) leftStack.push(rightStack.pop());
-            
-            // B: 커서 왼쪽에 있는 문자를 삭제
-            else if (cmd[0].equals("B") && !leftStack.isEmpty()) leftStack.pop();
-            
-            // P: 문자(cmd[1])를 커서 왼쪽에 추가
-            else if (cmd[0].equals("P")) leftStack.push(cmd[1].charAt(0));
+            if (cmd[0].equals("L") && !left.isEmpty()) right.add(left.pop());
+            else if (cmd[0].equals("D") && !right.isEmpty()) left.add(right.pop());
+            else if (cmd[0].equals("B") && !left.isEmpty()) left.pop();
+            else if (cmd[0].equals("P")) left.add(cmd[1].charAt(0));
         }
-
-        while (!leftStack.isEmpty()) rightStack.push(leftStack.pop());
 
         StringBuilder sb = new StringBuilder();
-        while (!rightStack.isEmpty()) sb.append(rightStack.pop());
+        while (!left.isEmpty()) right.add(left.pop());
+        while (!right.isEmpty()) sb.append(right.pop());
         System.out.println(sb);
     }
 }
