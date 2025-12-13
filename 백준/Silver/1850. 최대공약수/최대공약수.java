cc57da49fd57728d07a0b2a04cde7150513b44d2
Nageum Jeong <nagum0545@gmail.com>
2025-12-13 19:29:55 +0900

    [Silver I] Title: 최대공약수, Time: 296 ms, Memory: 41392 KB -BaekjoonHub

---
+++ 백준/Silver/1850. 최대공약수/최대공약수.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long g = gcd(A, B);
+
+        StringBuilder sb = new StringBuilder();
+        for (long i = 0; i < g; i++) {
+            sb.append(1);
+        }
+
+        System.out.println(sb);
+    }
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long tmp = b;
+            b = a % b;
+            a = tmp;
+        }
+        return a;
+    }
+}
