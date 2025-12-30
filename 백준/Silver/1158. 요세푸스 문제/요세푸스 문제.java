276f2d2ca15a85c0745ce3eea8174ef896292a09
Nageum Jeong <nagum0545@gmail.com>
2025-12-30 15:21:39 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 236 ms, Memory: 16724 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -8,25 +8,24 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        Deque<Integer> dq = new ArrayDeque<>();
+        Deque<Integer> q = new ArrayDeque<>();
         for (int i = 1; i <= N; i++) {
-            dq.add(i);
+            q.offer(i);
         }
 
         StringBuilder sb = new StringBuilder();
-        sb.append('<');
-        int cnt = 1;
-        while (!dq.isEmpty()) {
-            if (cnt == K) {
-                sb.append(dq.poll());
-                if (!dq.isEmpty()) sb.append(", ");
-                cnt = 1;
-            } else {
-                dq.add(dq.poll());
+        sb.append("<");
+        while (!q.isEmpty()) {
+            int cnt = 0;
+            while (cnt < K-1) {
+                q.offer(q.poll());
                 cnt++;
             }
+
+            sb.append(q.poll()).append(", ");
         }
-        sb.append('>');
+
+        sb.delete(sb.length()-2, sb.length()).append(">");
         System.out.println(sb);
     }
 }
