fae40b44609a8b0cdb1dd3f29b331e8963d43e6b
Nageum Jeong <nagum0545@gmail.com>
2025-12-30 15:11:22 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 28900 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -6,16 +6,19 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        Queue<Integer> q = new LinkedList<>();
+        Deque<Integer> dq = new ArrayDeque<>();
         for (int i = 1; i <= N; i++) {
-            q.offer(i);
+            dq.offer(i);
         }
 
-        while(q.size() > 1) {
-            q.poll(); // 1. 제일 위에 있는 카드 버리기
-            q.offer(q.poll()); // 2. 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮기기
+        while (dq.size() > 1) {
+            // 1. 제일 위에 있는 카드 버리기
+            dq.poll();
+
+            // 2. 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮기기
+            dq.offer(dq.poll());
         }
 
-        System.out.println(q.poll());
+        System.out.println(dq.peek());
     }
 }
