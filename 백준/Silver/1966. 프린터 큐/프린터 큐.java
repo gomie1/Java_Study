a565a76676b2e705d5c573e7e56f26eb78f4eab7
Nageum Jeong <nagum0545@gmail.com>
2025-12-30 15:43:37 +0900

    [Silver III] Title: 프린터 큐, Time: 124 ms, Memory: 14576 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -4,48 +4,41 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
         StringTokenizer st;
-        Queue<int[]> q = new LinkedList<>();
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < tc; i++) {
+        for (int tc = 0; tc < T; tc++) {
             st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
-            int idx = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
 
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                q.offer(new int[] {Integer.parseInt(st.nextToken()), j});
+            Deque<int[]> dq = new ArrayDeque<>();
+            Integer[] priority = new Integer[N];
+            for (int i = 0; i < N; i++) {
+                priority[i] = Integer.parseInt(st.nextToken());
+                dq.offer(new int[] {i, priority[i]});
             }
 
-            int ans = 0;
-            int cnt = 0;
-            while (!q.isEmpty()) {
-                int[] cur = q.poll();
-
-                boolean flag = true;
-                for (int[] d : q) {
-                    if (d[0] > cur[0]) {
-                        q.offer(cur);
-                        flag = false;
-                        break;
-                    }
+            Arrays.sort(priority, Collections.reverseOrder());
+            int idx = 0;
+            int cnt = 1;
+            while (!dq.isEmpty()) {
+                while (dq.peek()[1] != priority[idx]) {
+                    dq.offer(dq.poll());
                 }
 
-                if (flag) {
-                    cnt++;
-                    if (cur[1] == idx) {
-                        ans = cnt;
-                        break;
-                    }
+                int[] cur = dq.poll();
+                if (cur[0] == M) {
+                    sb.append(cnt).append('\n');
+                    break;
                 }
+                idx++;
+                cnt++;
             }
-
-            sb.append(ans).append('\n');
-            q.clear();
         }
 
-        System.out.print(sb);
+        System.out.println(sb);
     }
 }
