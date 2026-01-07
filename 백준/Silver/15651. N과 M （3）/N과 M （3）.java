8824ffa2bba3dbb5ba29aede8a61fc7d30af5990
Nageum Jeong <nagum0545@gmail.com>
2026-01-07 14:48:38 +0900

    [Silver III] Title: N과 M (3), Time: 396 ms, Memory: 67996 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -2,27 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int N, M, arr[];
     static StringBuilder sb = new StringBuilder();
-    static int n, m;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        dup_perm("", 0);
+        arr = new int[M];
+        permutation(0);
         System.out.println(sb);
     }
 
-    static void dup_perm(String ans, int depth) {
-        if (depth == m) {
-            sb.append(ans).append("\n");
+    static void permutation(int cnt) {
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append('\n');
             return;
         }
 
-        for (int i = 1; i <= n; i++) {
-            dup_perm(ans + Integer.toString(i) + " ", depth + 1);
+        for (int i = 1; i <= N; i++) {
+            arr[cnt] = i;
+            permutation(cnt+1);
         }
     }
 }
