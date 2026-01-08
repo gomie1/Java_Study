bab5504922151e2570cba007f4798e3e134d3f83
Nageum Jeong <nagum0545@gmail.com>
2026-01-08 18:54:10 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 16656 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M, numbers[];
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        numbers = new int[M];
+        combination(0, 1);
+        System.out.println(sb);
+    }
+
+    static void combination(int cnt, int start) {
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(numbers[i]).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            numbers[cnt] = i;
+            combination(cnt+1, i);
+        }
+    }
+}
