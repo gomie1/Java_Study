48fe2dfcdbbc0cb5adbac213242115de25d3b963
Nageum Jeong <nagum0545@gmail.com>
2026-01-11 00:18:44 +0900

    [Gold III] Title: 아기 상어, Time: 180 ms, Memory: 23756 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Main {
     static int N, map[][], shark;
-    static ArrayList<int[]> fishPos = new ArrayList<>();
 
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
@@ -25,9 +24,6 @@ public class Main {
                     x = i;
                     y = j;
                 }
-
-                // 물고기의 위치
-                if (map[i][j] >= 1 && map[i][j] <= 6) fishPos.add(new int[] {i, j});
             }
         }
 
