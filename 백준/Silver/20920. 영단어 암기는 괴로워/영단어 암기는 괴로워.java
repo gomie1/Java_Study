30e5df7a088b944f0baee732115f30a05a0f0db3
Nageum Jeong <nagum0545@gmail.com>
2026-01-24 20:54:22 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 612 ms, Memory: 36168 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -2,51 +2,47 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Voca implements Comparable<Voca> {
-        String voca;
+    static class Word implements Comparable<Word> {
+        String str;
         int len;
         int cnt;
 
-        public Voca(String voca, int len, int cnt) {
-            this.voca = voca;
+        Word(String str, int len, int cnt) {
+            this.str = str;
             this.len = len;
             this.cnt = cnt;
         }
 
         @Override
-        public int compareTo(Voca o) {
-            if (this.cnt != o.cnt) return o.cnt - this.cnt; // 횟수가 많은 순 (내림차순)
-            if (this.len != o.len) return o.len - this.len; // 길이가 긴 순 (내림차순)
-            return this.voca.compareTo(o.voca); // 사전 순서가 빠른 순 (오름차순)
+        public int compareTo(Word o) {
+            if (this.cnt != o.cnt) return o.cnt - this.cnt;
+            if (this.len != o.len) return o.len - this.len;
+            return this.str.compareTo(o.str);
         }
     }
-
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        HashMap<String, Integer> count = new HashMap<>();
+        HashMap<String, Integer> words = new HashMap<>();
         for (int i = 0; i < N; i++) {
-            String voca = br.readLine();
-            if (voca.length() < M) continue;
-            count.put(voca, count.getOrDefault(voca, 0) + 1);
+            String str = br.readLine();
+            if (str.length() < M) continue;
+            words.put(str, words.getOrDefault(str, 1) + 1);
         }
 
-        Voca[] vocas = new Voca[count.size()];
-        int idx = 0;
-        for (String key : count.keySet()) {
-            vocas[idx++] = new Voca(key, key.length(), count.get(key));
+        PriorityQueue<Word> pq = new PriorityQueue<>();
+        for (String key : words.keySet()) {
+            pq.offer(new Word(key, key.length(), words.get(key)));
         }
-
-        Arrays.sort(vocas);
 
         StringBuilder sb = new StringBuilder();
-        for (Voca v : vocas) {
-            sb.append(v.voca).append('\n');
+        while (!pq.isEmpty()) {
+            sb.append(pq.poll().str).append('\n');
         }
-
         System.out.println(sb);
     }
 }
