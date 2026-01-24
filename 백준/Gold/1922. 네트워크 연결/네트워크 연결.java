b4c191e400529d09ed234b892b853b042efe1da7
Nageum Jeong <nagum0545@gmail.com>
2026-01-24 20:52:42 +0900

    [Gold IV] Title: 네트워크 연결, Time: 316 ms, Memory: 47712 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -1,74 +1,56 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[] parents;
-
-    static class Edge implements Comparable<Edge> {
-        int start;
-        int end;
-        int cost;
-
-        public Edge(int s, int e, int c) {
-            this.start = s;
-            this.end = e;
-            this.cost = c;
-        }
-
-        @Override
-        public int compareTo(Edge o) {
-            return this.cost - o.cost;
-        }
-    }
-
-    private static boolean union(int a, int b) {
-        int aRoot = findSet(a);
-        int bRoot = findSet(b);
-
-        if(aRoot == bRoot) return false; // root 노드가 같다면 union 했을 때, 사이클이 발생하므로 union 불가능
-
-        // a, b의 root 노드가 다르다면 b의 root 노드를 a의 root노드로 설정 (Union!!)
-        parents[bRoot] = aRoot;
-        return true;
-    }
-
-    private static int findSet(int a) {
-        if(parents[a] < 0) return a; // parents[a]가 초기값인 -1이면 a가 root 노드인 것이므로 본인을 반환
-        return parents[a] = findSet(parents[a]); // a가 root 노드가 아니라면 a의 root 노드를 찾아서 a의 부모로 설정
-    }
+    static int N, parents[];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()); // 컴퓨터의 수
-        int M = Integer.parseInt(br.readLine()); // 연결의 수
+        N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
 
-        // 연결 정보 입력 받기
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
+           return o1[2] - o2[2];
+        });
         StringTokenizer st;
-        Edge[] edges = new Edge[M];
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            edges[i] = new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            pq.offer(new int[] {a, b, cost});
         }
 
-        // 비용이 낮은 순서대로 컴퓨터들의 연결을 정렬
-        Arrays.sort(edges);
-
-        // 각 컴퓨터의 부모를 -1로 초기화
         parents = new int[N+1];
-        Arrays.fill(parents, -1);
+        for (int i = 1; i <= N; i++) {
+            parents[i] = i;
+        }
 
-        int res = 0;
-        int cnt = 1; // 연결한 컴퓨터의 개수
-        for(Edge edge : edges) {
-            if(union(edge.start, edge.end)) {
-                res += edge.cost;
-                if(++cnt == N) break; // 모든 컴퓨터가 다 연결되었다면 종료!
+        int cnt = 0;
+        int ans = 0;
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            if (union(cur[0], cur[1])) {
+                ans += cur[2];
+                if (++cnt == N-1) break;
             }
         }
 
-        System.out.println(res);
+        System.out.println(ans);
+    }
+
+    static int findRoot(int x) {
+        if (parents[x] == x) return x;
+        return parents[x] = findRoot(parents[x]);
+    }
+
+    static boolean union(int a, int b) {
+        int aRoot = findRoot(a);
+        int bRoot = findRoot(b);
+        if (aRoot == bRoot) return false;
+
+        parents[bRoot] = aRoot;
+        return true;
     }
 }
