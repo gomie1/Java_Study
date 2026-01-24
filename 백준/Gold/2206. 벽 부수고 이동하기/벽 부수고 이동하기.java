cb8e5a935136a7c18c20818a3ecb76f5831a3cb0
Nageum Jeong <nagum0545@gmail.com>
2026-01-24 20:55:19 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 660 ms, Memory: 114256 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,17 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    private static int[] dx = {-1, 1, 0, 0};
-    private static int[] dy = {0, 0, -1, 1};
-    private static int N, M, res;
-    private static char[][] map;
-    private static boolean[][][] visited;
+    static int N, M, map[][], ans;
+    static boolean visited[][][];
+
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,51 +14,46 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        map = new char[N][M];
+        map = new int[N][M];
         for (int i = 0; i < N; i++) {
             String input = br.readLine();
-            map[i] = input.toCharArray();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = input.charAt(j) - '0';
+            }
         }
 
         visited = new boolean[N][M][2];
-        res = -1;
-        bfs(0, 0, 1);
-        System.out.println(res);
+        ans = -1;
+        bfs(0, 0);
+        System.out.println(ans);
     }
 
-    private static void bfs(int start_x, int start_y, int cnt) {
-        //Queue<int[]> queue = new LinkedList<>();
-        PriorityQueue<int []> queue = new PriorityQueue<>((o1, o2) -> {
-            int abs1 = o1[2];
-            int abs2 = o2[2];
+    static void bfs(int sx, int sy) {
+        ArrayDeque<int[]> dq = new ArrayDeque<>();
+        dq.offer(new int[] {sx, sy, 1, 0}); // {x, y, dist, isBreak}
 
-            return abs1 > abs2 ? 1 : -1;
-        });
-        visited[start_x][start_y][0] = true;
-        queue.offer(new int[] {start_x, start_y, cnt, 0});
-        while(!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            if(cur[0] == N-1 && cur[1] == M-1) {
-                res = cur[2];
-                return;
+        while (!dq.isEmpty()) {
+            int[] cur = dq.poll();
+            if (cur[0] == N-1 && cur[1] == M-1) {
+                ans = cur[2];
+                break;
             }
 
             for (int i = 0; i < 4; i++) {
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
 
-                if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-                // 다음 위치가 벽일 때 (부수고 지나가기 vs 패스하기)
-                if(map[nx][ny] == '1') {
-                    if(cur[3] == 0) { // 아직 벽을 부순 적이 없다면
-                        visited[nx][ny][1] = true; // 벽을 부수고 지나감
-                        queue.offer(new int[] {nx, ny, cur[2]+1, 1});
-                    }
-                } else { // 벽이 아니면 지나가기
-                    if(!visited[nx][ny][cur[3]]) {
-                        visited[nx][ny][cur[3]] = true;
-                        queue.offer(new int[] {nx, ny, cur[2]+1, cur[3]});
-                    }
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M || visited[nx][ny][cur[3]]) continue;
+
+                if (map[nx][ny] == 0) {
+                    visited[nx][ny][cur[3]] = true;
+                    dq.offer(new int[] {nx, ny, cur[2]+1, cur[3]});
+                    continue;
+                }
+
+                if (map[nx][ny] == 1 && cur[3] == 0) {
+                    visited[nx][ny][1] = true;
+                    dq.offer(new int[] {nx, ny, cur[2]+1, 1});
                 }
             }
         }
