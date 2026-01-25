df5b8581595dadd266f8214ac953c39fb9053b03
Nageum Jeong <nagum0545@gmail.com>
2026-01-25 23:10:10 +0900

    [Gold IV] Title: LCS, Time: 136 ms, Memory: 18556 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,19 +1,20 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str1 = br.readLine();
-        String str2 = br.readLine();
+        String s1 = br.readLine();
+        String s2 = br.readLine();
 
-        int[][] dp = new int[str1.length()+1][str2.length()+1];
-        for (int i = 1; i < str1.length()+1; i++) {
-            for (int j = 1; j < str2.length()+1; j++) {
-                if (str1.charAt(i-1) == str2.charAt(j-1)) dp[i][j] = dp[i-1][j-1] + 1;
-                else dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+        int[][] dp = new int[s1.length()+1][s2.length()+1];
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                if (s1.charAt(i-1) == s2.charAt(j-1)) dp[i][j] = dp[i-1][j-1] + 1;
+                else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
             }
         }
 
-        System.out.println(dp[str1.length()][str2.length()]);
+        System.out.println(dp[s1.length()][s2.length()]);
     }
 }
