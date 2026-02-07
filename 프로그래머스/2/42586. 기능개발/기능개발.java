b903102a18629a7538c53e4b2e0440cf1020762f
Nageum Jeong <nagum0545@gmail.com>
2026-02-08 01:09:04 +0900

    [level 2] Title: 기능개발, Time: 0.22 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,34 +2,33 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        // 1. 각 작업별 완성하기까지 걸리는 시간 계산하기
-        int[] days = new int[progresses.length];
+        Stack<Integer> stack = new Stack<>();
+        List<Integer> res = new ArrayList<>();
+        int cnt = 0;
         for (int i = 0; i < progresses.length; i++) {
-            if ((100 - progresses[i]) % speeds[i] == 0) days[i] = (100 - progresses[i]) / speeds[i];
-            else days[i] = ((100 - progresses[i]) / speeds[i]) + 1;
-        }
-        
-        int idx = 0;
-        int maxDay = days[idx++];
-        int cnt = 1;
-        List<Integer> cntList = new ArrayList<>();
-        while (idx < days.length) {
-            if (days[idx] <= maxDay) cnt++;
-            else {
-                cntList.add(cnt);
-                cnt = 1;
-                maxDay = days[idx];
+            int d = (100 - progresses[i]) / speeds[i];
+            int date = (100 - progresses[i]) % speeds[i] == 0 ? d : d+1;
+            
+            if (stack.isEmpty()) {
+                stack.add(date);
+                cnt++;
+                continue;
             }
             
-            idx++;
+            if (date <= stack.peek()) cnt++;
+            else {
+                stack.pop();
+                res.add(cnt);
+                stack.add(date);
+                cnt = 1;
+            }
         }
-        cntList.add(cnt);
+        res.add(cnt);
         
-        int[] ans = new int[cntList.size()];
-        for (int i = 0; i < cntList.size(); i++) {
-            ans[i] = cntList.get(i);
+        int[] answer = new int[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            answer[i] = res.get(i);
         }
-        
-        return ans;
+        return answer;
     }
 }
