e9612b6ad656cd6b23b49e6175fc83a1c76ce4e8
Nageum Jeong <nagum0545@gmail.com>
2026-02-16 19:29:26 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 292.55 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,16 +2,18 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String, Integer> players = new HashMap<>();
-        for (String s : completion) {
-            players.put(s, players.getOrDefault(s, 0) + 1);
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        String answer = "";
+        for (int i = 0; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i])) {
+                answer = participant[i];
+                break;
+            }
         }
         
-        for (String s : participant) {
-            if (!players.containsKey(s) || players.get(s) == 0) return s;
-            players.put(s, players.get(s) - 1);
-        }
-        
-        return "";
+        if (answer.isEmpty()) answer = participant[participant.length - 1];
+        return answer;
     }
 }
