b8f84ac895d51b2751bf801403e61600dba6e391
Nageum Jeong <nagum0545@gmail.com>
2026-02-16 22:23:09 +0900

    [level 2] Title: 전화번호 목록, Time: 350.08 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,20 +2,19 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
+        // 전화번호가 문자열로 되어있기 때문에, 사전 순으로 정렬됨
+        // 사전 순 정렬은 접두어가 같으면 이어서 나오기 때문에 앞뒤만 확인하면 됨
+        // ex. "가" > "가나" > "가나다", "1" > "11" > "12" > "113" > "4"
         Arrays.sort(phone_book);
-        HashSet<String> numbers = new HashSet<>();
-        numbers.add(phone_book[0]);
         
-        StringBuilder sb;
+        boolean answer = true;
         for (int i = 1; i < phone_book.length; i++) {
-            sb = new StringBuilder();
-            for (char c : phone_book[i].toCharArray()) {
-                sb.append(c);
-                if (numbers.contains(sb.toString())) return false;
+            if (phone_book[i].startsWith(phone_book[i-1])) {
+                answer = false;
+                break;
             }
-            numbers.add(phone_book[i]);
         }
-
-        return true;
+        
+        return answer;
     }
 }
