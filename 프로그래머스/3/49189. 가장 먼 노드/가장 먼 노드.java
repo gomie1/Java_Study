d4ffe7156da90bdde1cf3c5c5da96e3926bb8f04
Nageum Jeong <nagum0545@gmail.com>
2026-02-21 14:42:27 +0900

    [level 3] Title: 가장 먼 노드, Time: 61.09 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,38 +1,50 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
+    static ArrayList<Integer>[] graph;
+    static int[] dist;
+    
     public int solution(int n, int[][] edge) {
-        ArrayList<Integer>[] graph = new ArrayList[n+1];
-        for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
+        graph = new ArrayList[n+1];
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
         
         for (int[] e : edge) {
             graph[e[0]].add(e[1]);
             graph[e[1]].add(e[0]);
         }
         
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(1);
-        int[] dist = new int[n+1];
-        int max = 0;
+        dist = new int[n+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[1] = 0;
         
-        while (!q.isEmpty()) {
-            int cur = q.poll();
+        int maxDist = dijkstra();
+        int answer = 0;
+        for (int i = 2; i <= n; i++) {
+            if (dist[i] == maxDist) answer++;
+        }
+        
+        return answer;
+    }
+    
+    static int dijkstra() {
+        Deque<int[]> dq = new ArrayDeque<>();
+        dq.add(new int[] {1, 0}); // {node, dist}
+        
+        int maxDist = 0;
+        while (!dq.isEmpty()) {
+            int[] cur = dq.poll();
+            if (maxDist < cur[1]) maxDist = cur[1];
             
-            for (int nxt : graph[cur]) {
-                if (nxt != 1 && dist[nxt] == 0) {
-                    dist[nxt] = dist[cur] + 1;
-                    q.offer(nxt);
-                    if (max < dist[nxt]) max = dist[nxt];
+            for (int nxt : graph[cur[0]]) {
+                if (cur[1] + 1 < dist[nxt]) {
+                    dist[nxt] = cur[1] + 1;
+                    dq.add(new int[] {nxt, dist[nxt]});
                 }
             }
         }
         
-        int answer = 0;
-        for (int d : dist) {
-            if (d == max) answer++;
-        }
-        
-        return answer;
+        return maxDist;
     }
 }
