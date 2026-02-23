6302f024353c0bab15da0d9f8c83980333bcb94f
Nageum Jeong <nagum0545@gmail.com>
2026-02-23 12:34:09 +0900

    [Gold IV] Title: 키 순서, Time: 592 ms, Memory: 36532 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static final int INF = 1_000_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        boolean[][] check = new boolean[N+1][N+1];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            check[a][b] = true;
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (check[i][k] && check[k][j]) check[i][j] = true;
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 1; i <= N; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= N; j++) {
+                if (i == j) continue;
+                if (check[i][j] || check[j][i]) cnt++;
+            }
+
+            // 본인을 제외한 모든 사람과의 관계가 있다면 순위를 알 수 있음
+            if (cnt == N-1) ans++;
+        }
+
+        System.out.println(ans);
+    }
+}
