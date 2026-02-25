1d672a0f7dda43e5563b35f85e70f43bdb5f0660
Nageum Jeong <nagum0545@gmail.com>
2026-02-25 17:37:52 +0900

    [Silver I] Title: 숨바꼭질, Time: 132 ms, Memory: 16772 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -2,43 +2,47 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int ans = 0;
+    static int N, K;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-        if (K <= N) {
-            System.out.println(N - K);
-            return;
-        }
-
-        find(N, K, 0);
-        System.out.println(ans);
+        System.out.println(bfs());
     }
 
-    static void find(int X, int k, int time) {
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {X, time});
+    static int bfs() {
+        Deque<int[]> dq = new ArrayDeque<>();
+        dq.offer(new int[] {N, 0});
         boolean[] visited = new boolean[100001];
-        visited[X] = true;
+        visited[N] = true;
 
-        while (true) {
-            int[] cur = q.poll();
-            if (cur[0] == k) {
+        int ans = 0;
+        while (!dq.isEmpty()) {
+            int[] cur = dq.poll();
+            if (cur[0] == K) {
                 ans = cur[1];
                 break;
             }
 
-            int[] move = {cur[0] + 1, cur[0] - 1, cur[0] * 2};
-            for (int next : move) {
-                if (next >= 0 && next <= 100000 && !visited[next]) {
-                    visited[next] = true;
-                    q.offer(new int[] {next, cur[1] + 1});
-                }
+            if (cur[0] * 2 <= 100000 && !visited[2*cur[0]]) {
+                visited[2*cur[0]] = true;
+                dq.offer(new int[] {2*cur[0], cur[1]+1});
+            }
+
+            if (cur[0] + 1 <= 100000 && !visited[cur[0]+1]) {
+                visited[cur[0] + 1] = true;
+                dq.offer(new int[] {cur[0]+1, cur[1]+1});
+            }
+
+            if (cur[0] - 1 >= 0 && !visited[cur[0]-1]) {
+                visited[cur[0] - 1] = true;
+                dq.offer(new int[] {cur[0]-1, cur[1]+1});
             }
         }
+
+        return ans;
     }
 }
