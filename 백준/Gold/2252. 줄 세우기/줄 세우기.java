8e3ae2538be744213c98cd53edf585d093041c8e
Nageum Jeong <nagum0545@gmail.com>
2026-02-25 18:50:12 +0900

    [Gold III] Title: 줄 세우기, Time: 392 ms, Memory: 45576 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, degree[];
+    static int N, M, prev[];
     static ArrayList<Integer>[] graph;
     static StringBuilder sb = new StringBuilder();
 
@@ -10,34 +10,31 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
         graph = new ArrayList[N+1];
         for (int i = 1; i <= N; i++) {
             graph[i] = new ArrayList<>();
         }
-        degree = new int[N+1];
+        prev = new int[N+1];
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
             graph[a].add(b);
-            degree[b]++;
+            prev[b]++;
         }
 
-        lineup();
+        topology();
         System.out.println(sb);
     }
 
-    static void lineup() {
+    static void topology() {
+        // 1. 진입 차수가 0인 학생 찾기
         Deque<Integer> dq = new ArrayDeque<>();
-        boolean[] visited = new boolean[N+1];
         for (int i = 1; i <= N; i++) {
-            if (degree[i] == 0) {
-                visited[i] = true;
-                dq.offer(i);
-            }
+            if (prev[i] == 0) dq.add(i);
         }
 
         while (!dq.isEmpty()) {
@@ -45,10 +42,7 @@ public class Main {
             sb.append(cur).append(" ");
 
             for (int nxt : graph[cur]) {
-                if (!visited[nxt] && --degree[nxt] == 0) {
-                    visited[nxt] = true;
-                    dq.offer(nxt);
-                }
+                if (--prev[nxt] == 0) dq.add(nxt);
             }
         }
     }
