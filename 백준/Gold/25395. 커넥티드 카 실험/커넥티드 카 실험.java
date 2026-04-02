e2695a0e27ce02360d5085a973406fd8ca7fd04e
Nageum Jeong <nagum0545@gmail.com>
2026-04-02 21:46:27 +0900

    [Gold III] Title: 커넥티드 카 실험, Time: 1096 ms, Memory: 263880 KB -BaekjoonHub

--- 백준/Gold/25395. 커넥티드 카 실험/커넥티드 카 실험.java
+++ 백준/Gold/25395. 커넥티드 카 실험/커넥티드 카 실험.java
@@ -2,51 +2,62 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int S = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken()); // 커넥티드 카 개수
+        int S = Integer.parseInt(st.nextToken()); // 처음 연결할 커넥티드 카 번호
 
-        st = new StringTokenizer(br.readLine());
+        // 초기 위치
         int[] x = new int[N+1];
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
             x[i] = Integer.parseInt(st.nextToken());
         }
 
-        st = new StringTokenizer(br.readLine());
+        // 연료량
         int[] h = new int[N+1];
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
             h[i] = Integer.parseInt(st.nextToken());
         }
 
         Queue<Integer> q = new LinkedList<>();
-        boolean[] visited = new boolean[N+1];
-        q.offer(S);
-        visited[S] = true;
+        q.add(S);
+        boolean[] isConnected = new boolean[N+1];
+        isConnected[S] = true;
 
-        int left = Math.max(0, S-1);
-        int right = Math.min(S+1, N);
+        int left = S;
+        int right = S;
         while (!q.isEmpty()) {
             int cur = q.poll();
-            int[] reach = {x[cur]-h[cur], x[cur]+h[cur]};
 
-            // 왼쪽 체크
-            while (left >= 1 && x[left] >= reach[0]) {
-                q.add(left);
-                visited[left--] = true;
+            // 현재 차가 전파를 보낼 수 있는 최소, 최대 좌표
+            int minPos = x[cur] - h[cur];
+            int maxPos = x[cur] + h[cur];
+
+            // 1. 왼쪽 경계선 확장
+            while (left - 1 > 0 && x[left - 1] >= minPos) {
+                left--;
+                if (!isConnected[left]) {
+                    isConnected[left] = true;
+                    q.add(left);
+                }
             }
 
-            // 오른쪽 체크
-            while (right <= N && x[right] <= reach[1]) {
-                q.add(right);
-                visited[right++] = true;
+            // 2. 오르쪽 경계선 확장
+            while (right + 1 <= N && x[right + 1] <= maxPos) {
+                right++;
+                if (!isConnected[right]) {
+                    isConnected[right] = true;
+                    q.add(right);
+                }
             }
         }
 
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= N; i++) {
-            if (visited[i]) sb.append(i).append(" ");
+            if (isConnected[i]) sb.append(i).append(" ");
         }
 
         System.out.println(sb);
