50831d395802e98424c59f32e8769a87b4e53fb0
Nageum Jeong <nagum0545@gmail.com>
2026-04-02 18:43:43 +0900

    [Silver V] Title: 모바일 광고 입찰, Time: 408 ms, Memory: 41740 KB -BaekjoonHub

--- 백준/Silver/31246. 모바일 광고 입찰/모바일 광고 입찰.java
+++ 백준/Silver/31246. 모바일 광고 입찰/모바일 광고 입찰.java
@@ -2,29 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, K, A[], B[];
-
     public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        A = new int[N]; // MOLOCO가 제시한 입찰 가격
-        B = new int[N]; // 입찰가를 제외한 입찰가 중 최고가
         int cnt = 0; // 낙찰 지면 수
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            A[i] = Integer.parseInt(st.nextToken());
-            B[i] = Integer.parseInt(st.nextToken());
+            int A = Integer.parseInt(st.nextToken()); // MOLOCO가 제시한 입찰 가격
+            int B = Integer.parseInt(st.nextToken()); // 입찰가를 제외한 입찰가 중 최고가
 
-            if (A[i] >= B[i]) cnt++;
-            else pq.add(B[i] - A[i]);
+            if (A >= B) cnt++;
+            else pq.add(B - A);
         }
 
         int ans = 0;
-        while (cnt < K && !pq.isEmpty()) {
+        while (cnt < K) {
             ans = pq.poll();
             cnt++;
         }
