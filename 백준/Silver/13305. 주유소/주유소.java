6bdd6f9326c561553d85b56d3c27cbc4579bced1
Nageum Jeong <nagum0545@gmail.com>
2026-04-03 16:54:45 +0900

    [Silver III] Title: 주유소, Time: 328 ms, Memory: 30876 KB, Score: 17 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -2,42 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine()); // 도시의 개수
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] dist = new int[N-1];
+        int[] dist = new int[N-1]; // 도로의 길이
         for (int i = 0; i < N-1; i++) {
             dist[i] = Integer.parseInt(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine());
-        int[] price = new int[N];
+        int[] price = new int[N]; // 주유소의 리터당 가격
         for (int i = 0; i < N; i++) {
             price[i] = Integer.parseInt(st.nextToken());
         }
 
-        long ans = 0; // 총 비용
-        int sumDist = 0; // 누적 거리
-        int cur = 0; // 현재 도시의 인덱스
-        while (cur < N-1) {
-            if (price[cur] > price[cur+1]) {
-                ans += (long) price[cur] * dist[cur];
-                cur++;
-            } else {
-                int cnt = 0; // 주유를 건너뛸 도시의 개수
-                for (int i = cur; i < N-1; i++) {
-                    if (price[cur] > price[i]) break;
-                    sumDist += dist[i];
-                    cnt++;
-                }
-                ans += (long) price[cur] * sumDist;
-                cur += cnt;
-                sumDist = 0;
-            }
+        int[] cost = new int[N];
+        cost[0] = price[0];
+        for (int i = 1; i < N; i++) {
+            if (price[i-1] < price[i]) cost[i] = cost[i-1];
+            else cost[i] = price[i];
         }
 
-        System.out.println(ans);
+        long total = 0;
+        for (int i = 0; i < N-1; i++) {
+            total += (long) cost[i] * dist[i];
+        }
+
+        System.out.println(total);
     }
 }
