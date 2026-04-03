0b2d892a53a7d860d2eb36559a9032766604f2a5
Nageum Jeong <nagum0545@gmail.com>
2026-04-03 13:12:43 +0900

    [Gold V] Title: 평범한 배낭, Time: 140 ms, Memory: 15564 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -2,20 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken()); // 물품의 수
+        int K = Integer.parseInt(st.nextToken()); // 준서가 버틸 수 있는 무게
 
-        int[] dp = new int[K+1];
-        for (int i = 1; i <= N; i++) {
+        int[] dp = new int[K+1]; // dp[i] = 배낭에 ikg까지 담을 수 있을 때, 가치의 최대값
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int weight = Integer.parseInt(st.nextToken());
             int value = Integer.parseInt(st.nextToken());
 
-            for (int w = K; w >= weight; w--) {
-                dp[w] = Math.max(dp[w], dp[w-weight] + value);
+            // 경우는 배낭에 현재 물건을 넣는 경우와 안넣는 경우 2가지가 존재
+            // ex. 물건의 무게가 3, 가치가 6인 상황 -> dp[7] = 현재 물건을 넣는 경우(dp[7-3] + 6)와 안넣는 경우(dp[7]) 중 큰 값으로 선택
+            for (int j = K; j >= weight; j--) {
+                dp[j] = Math.max(dp[j], dp[j - weight] + value);
             }
         }
 
