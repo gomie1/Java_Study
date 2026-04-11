a2b283fe2c72656a3b0b92d4d4a2a84ddecd9024
Nageum Jeong <nagum0545@gmail.com>
2026-04-11 19:13:45 +0900

    [Gold V] Title: 로봇 청소기, Time: 108 ms, Memory: 14428 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,80 +1,89 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N, M, arr[][], x, y, d, res;
+    static int N, M, arr[][];
 
-    // 0: 북, 1: 동, 2: 남, 3: 서
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-
-    public static void main(String[] args) throws IOException {
+    public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        x = Integer.parseInt(st.nextToken());
-        y = Integer.parseInt(st.nextToken());
-        d = Integer.parseInt(st.nextToken());
+        int sx = Integer.parseInt(st.nextToken());
+        int sy = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
 
-        arr = new int[N][M]; // 0: 청소되지 않은 빈 칸, 1: 벽
+        arr = new int[N][M];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = Integer.parseInt(st.nextToken()); // 0: 청소 X 빈칸, 1: 벽
             }
         }
 
-        res = 0;
-        sol();
-        System.out.println(res);
+        System.out.println(cleaning(sx, sy, d));
     }
 
-    static void sol() {
-        while(true){
-            // 1. 현재 칸이 아직 청소되지 않은 경우, 현재 칸을 청소 (0 -> -1)
-            if(arr[x][y] == 0) {
-                res++; // 청소한 칸 개수 증가
-                arr[x][y] = -1; // 청소한 칸임을 표시하기 위해 -1로 변경
-            }
+    static int cleaning(int x, int y, int d) {
+        // 0: 북쪽, 1: 동쪽, 2: 남쪽, 3: 서쪽
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
 
-            // 2. 현재 칸의 주변 4칸 중 청소되지 않은 칸이 존재하는지 확인
+        int cnt = 0;
+        while (true) {
+            // 1. 현재 칸이 아직 청소되지 않은 경우, 현재 칸을 청소한다.
+            if (arr[x][y] == 0) {
+                arr[x][y] = 2; // 2: 청소 표시
+                cnt++;
+            }
+            
+            // 현재 칸의 주변 4칸 중 청소되지 않은 빈 칸의 존재 여부 확인
             boolean flag = false;
             for (int i = 0; i < 4; i++) {
-                int ndir = (d+3) %4;
-                int nx = x + dx[ndir];
-                int ny = y + dy[ndir];
-                if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-                if(arr[nx][ny] == 0) {
-                    flag = true; // 청소되지 않은 칸이 존재한다면 해당 방향으로 flag 체크
-                    d = ndir;
-                    x = nx;
-                    y = ny;
-                    break;
-                }
-                d = (d+3) % 4;
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M || arr[nx][ny] != 0) continue;
+
+                flag = true;
+                break;
             }
 
-            // 3. 주변 4칸 중 청소되지 않은 빈 칸이 없는 경우
-            if(!flag) {
-                // 바라보는 방향을 유지한 채로 한 칸 후진할 수 있다면 후진
-                int ndir = (d+2) % 4;
+            // 2. 현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 없는 경우
+            if (!flag) {
+                int nx = x + dx[(d+2) % 4];
+                int ny = y + dy[(d+2) % 4];
 
-                int nx = x + dx[ndir];
-                int ny = y + dy[ndir];
-                if(nx >= 0 && nx < N && ny >= 0 && ny < M && arr[nx][ny] != 1) { // 후진할 수 있는 경우 (벽이 아닌 경우)
-                    // 현재 위치를 후진한 위치로 갱신
+                // 2-1. 바라보는 방향을 유지한 채로 한 칸 후진할 수 있다면 한 칸 후진하고 1번으로 돌아간다.
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M && arr[nx][ny] != 1) {
                     x = nx;
                     y = ny;
                 }
-                else { // 후진할 수 없다면 작동 중지 (종료)
-                    return;
+
+                // 2-2. 바라보는 방향의 뒤쪽 칸이 벽이라 후진할 수 없다면 작동을 멈춘다.
+                else break;
+            }
+
+            // 3. 현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 있는 경우
+            if (flag) {
+                for (int i = 0; i < 4; i++) {
+                    // 3-1. 반시계 방향으로 90도 회전한다.
+                    d = (d+3) % 4;
+
+                    // 3-2. 바라보는 뱡향을 기준으로 앞쪽 칸이 청소되지 않은 빈 칸인 경우 한 칸 전진한다.
+                    int nx = x + dx[d];
+                    int ny = y + dy[d];
+                    if (nx >= 0 && nx < N && ny >= 0 && ny < M && arr[nx][ny] == 0) {
+                        x = nx;
+                        y = ny;
+                        break;
+                    }
                 }
             }
         }
+
+        return cnt;
     }
 }
