16940b0cc26cd6a1a43a514073759b32c979145b
Nageum Jeong <nagum0545@gmail.com>
2026-04-16 13:54:06 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,48 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, map[][];
-
-    public static void main(String[] args) throws IOException {
+    public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        map = new int[N][M];
+        int[][] maze = new int[N][M];
         for (int i = 0; i < N; i++) {
             String input = br.readLine();
             for (int j = 0; j < M; j++) {
-                map[i][j] = input.charAt(j) - '0';
+                maze[i][j] = input.charAt(j) - '0';
             }
         }
 
-        System.out.println(bfs(0, 0));
-    }
-
-    static int bfs(int sx, int sy) {
-        Deque<int[]> dq = new ArrayDeque<>();
-        dq.offer(new int[] {sx, sy ,1});
-
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] {0, 0});
         int[][] ans = new int[N][M];
-        ans[sx][sy] = 1;
+        ans[0][0] = 1;
 
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
-        while (!dq.isEmpty()) {
-            int[] cur = dq.poll();
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
 
             for (int i = 0; i < 4; i++) {
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
 
-                if (nx < 0 || nx >= N || ny < 0 || ny >= M || ans[nx][ny] != 0 || map[nx][ny] == 0) continue;
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M || maze[nx][ny] == 0 || ans[nx][ny] != 0) continue;
 
-                ans[nx][ny] = cur[2] + 1;
-                dq.offer(new int[] {nx, ny, ans[nx][ny]});
+                q.add(new int[] {nx, ny});
+                ans[nx][ny] = ans[cur[0]][cur[1]] + 1;
             }
         }
 
-        return ans[N-1][M-1];
+        System.out.println(ans[N-1][M-1]);
     }
 }
