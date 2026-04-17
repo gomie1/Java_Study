df2d49eb809976d5466df1a0fd00926e97682a66
Nageum Jeong <nagum0545@gmail.com>
2026-04-17 12:18:20 +0900

    [Gold V] Title: 토마토, Time: 568 ms, Memory: 128176 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -2,56 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, mtrx[][], cnt, ans;
-    static Queue<int[]> queue;
-
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-
-    public static void main(String[] args) throws IOException {
+    public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 
-        mtrx = new int[N][M];
-        queue = new LinkedList<>();
-        cnt = 0; // 익지 않은 토마토의 개수
-        ans = 0; // 날짜
+        int[][] tomato = new int[N][M];
+        Queue<int[]> q = new LinkedList<>(); // {x, y, day}
+        int cnt = 0; // 덜익은 토마토의 개수
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                mtrx[i][j] = Integer.parseInt(st.nextToken());
-                if (mtrx[i][j] == 1) queue.offer(new int[] {i, j, 0});
-                else if (mtrx[i][j] == 0) cnt++;
+                tomato[i][j] = Integer.parseInt(st.nextToken());
+                if (tomato[i][j] == 0) cnt++;
+                else if (tomato[i][j] == 1) q.add(new int[] {i, j, 0});
             }
         }
 
+        // 이미 모든 토마토가 다 익었다면 시간이 걸리지 않음
         if (cnt == 0) {
-            System.out.println(ans);
+            System.out.println(0);
             return;
         }
 
-        bfs();
-        if (cnt == 0) System.out.println(ans);
-        else System.out.println(-1);
-    }
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
 
-    static void bfs() {
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            if (ans < cur[2]) ans = cur[2];
-
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
             for (int i = 0; i < 4; i++) {
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
 
-                if (nx < 0 || nx >= N || ny < 0 || ny >= M || mtrx[nx][ny] != 0) continue;
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M || tomato[nx][ny] != 0) continue;
 
-                queue.offer(new int[] {nx, ny, cur[2] + 1});
-                mtrx[nx][ny] = 1;
+                q.add(new int[] {nx, ny, cur[2]+1});
+                tomato[nx][ny] = 1;
                 cnt--;
+
+                if (cnt == 0) {
+                    System.out.println(cur[2]+1);
+                    return;
+                }
             }
         }
+
+        System.out.println(-1);
     }
 }
