0e87303d18be4fcb1063b35b1ae834d1234e0fc8
Nageum Jeong <nagum0545@gmail.com>
2026-04-21 18:07:41 +0900

    [Gold IV] Title: 동전 1, Time: 112 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] coin = new int[n];
+        for (int i = 0; i < n; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(coin);
+
+        int[] dp = new int[k+1];
+        dp[0] = 1; // 동전 가치의 합이 0이 되는 경우는 아무 동전도 선택하지 않는 경우 1가지
+        for (int i = 0; i < n; i++) {
+            for (int j = coin[i]; j <= k; j++) {
+                dp[j] = dp[j] + dp[j - coin[i]];
+            }
+        }
+
+        System.out.println(dp[k]);
+    }
+}
