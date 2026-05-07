20fe6e92b861b4b0b0001442bc3ff2737954bb0c
Nageum Jeong <nagum0545@gmail.com>
2026-05-07 18:48:37 +0900

    [level 2] Title: H-Index, Time: 0.58 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,12 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        Arrays.sort(citations);
-        int len = citations.length;
+        int n = citations.length; // 논문의 수
+        Arrays.sort(citations); // [0, 1, 3, 5, 6]
         
-        for(int i = 0; i < len; i++) {
-            int h = len - i;
-            if(citations[i] >= h) return h;
+        for (int i = 0; i < n; i++) {
+            int h = n - i; // 현재 논문을 포함하여, 이보다 인용 횟수가 같거나 많은 논문의 개수
+            if (h <= citations[i]) return h; // 현재 논문의 인용 횟수가 h번 이상인지 확인
         }
         
         return 0;
