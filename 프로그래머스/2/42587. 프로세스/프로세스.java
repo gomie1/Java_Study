11f5562f90eceaf4777e555e941f3c96b92a7dea
Nageum Jeong <nagum0545@gmail.com>
2026-05-08 03:05:47 +0900

    [level 2] Title: 프로세스, Time: 0.84 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,23 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        Deque<int[]> dq = new ArrayDeque<>();
-        for (int i = 0; i < priorities.length; i++) {
-            dq.add(new int[] {i, priorities[i]});
+        int n = priorities.length;
+        
+        Queue<int[]> process = new LinkedList<>(); // {프로세스 번호, 우선순위}
+        for (int i = 0; i < n; i++) {
+            process.add(new int[] {i, priorities[i]});
         }
         
         Arrays.sort(priorities);
-        int idx = priorities.length - 1;
         
-        int answer = 0;
-        while (true) {
-            int[] cur = dq.pollFirst();
+        int idx = n-1;
+        int answer = 1; // 실행 횟수
+        while (!process.isEmpty()) {
+            int[] cur = process.poll();
             
-            if (cur[1] < priorities[idx]) dq.addLast(cur);
+            if (cur[1] < priorities[idx]) process.add(new int[] {cur[0], cur[1]});
             else {
-                idx--;
-                answer++;
                 if (cur[0] == location) break;
+                answer++;
+                idx--;
             }
         }
         
