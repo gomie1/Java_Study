555fe1a47553a9a2d754613336ac2791feda9dc0
Nageum Jeong <nagum0545@gmail.com>
2026-05-08 17:28:21 +0900

    [level 2] Title: 모음 사전, Time: 28.71 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,24 +1,24 @@
 import java.util.*;
 
 class Solution {
-    static char[] alpha = {'A', 'E', 'I', 'O', 'U'};
-    static HashMap<String, Integer> dictionary;
-    static int num;
+    static List<String> dictionary;
+    static char[] alphabet = {'A', 'E', 'I', 'O', 'U'};
     
     public int solution(String word) {
-        dictionary = new HashMap<>();
-        num = 1;
-        makeWords(0, "");
-        return dictionary.get(word);
+        // 사전 만들기
+        dictionary = new ArrayList<>();
+        makeDictionary(0, "");
+        Collections.sort(dictionary);
+        
+        return dictionary.indexOf(word) + 1;
     }
     
-    static void makeWords(int cnt, String word) {
-        if (!word.isEmpty() && !dictionary.containsKey(word)) dictionary.put(word, num++);
-        
+    static void makeDictionary(int cnt, String word) {
+        if (cnt > 0) dictionary.add(word);
         if (cnt == 5) return;
         
         for (int i = 0; i < 5; i++) {
-            makeWords(cnt+1, word+alpha[i]);
+            makeDictionary(cnt+1, word+alphabet[i]);
         }
     }
 }
