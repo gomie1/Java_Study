02f3be1bb50a3456b5ff9947abf462eaec045b57
Nageum Jeong <nagum0545@gmail.com>
2026-05-08 14:08:33 +0900

    [level 2] Title: 소수 찾기, Time: 18.96 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,44 +1,38 @@
 import java.util.*;
 
 class Solution {
-    static HashSet<Integer> set;
-    static boolean[] visited;
+    static Set<Integer> set = new HashSet<>();
+    static boolean[] isSelected;
     
     public int solution(String numbers) {
-        // 1. dfs(순열)를 통해 만들 수 있는 모든 숫자 생성
-        set = new HashSet<Integer>();
-        visited = new boolean[numbers.length()];
-        dfs(numbers.length(), "", numbers);
+        isSelected = new boolean[numbers.length()];
+        permutation("", numbers);
         
-        // 2. 소수 판별하기
         int answer = 0;
-        Iterator<Integer> iter = set.iterator();
-        while (iter.hasNext()) {
-            if (isPrime(iter.next())) answer++;
+        for (int n : set) {
+            if (isPrime(n)) answer++;
         }
         
         return answer;
     }
     
-    static void dfs(int n, String number, String origin) {
-        if (!number.equals("")) {
-            set.add(Integer.parseInt(number));
-        }
+    static void permutation(String cur, String numbers) {
+        if (!cur.equals("")) set.add(Integer.parseInt(cur));
         
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                dfs(n, number+origin.charAt(i), origin);
-                visited[i] = false;
+        for (int i = 0; i < numbers.length(); i++) {
+            if (!isSelected[i]) {
+                isSelected[i] = true;
+                permutation(cur+numbers.charAt(i), numbers);
+                isSelected[i] = false;
             }
         }
     }
     
-    static boolean isPrime(int n) {
-        if (n < 2) return false;
+    static boolean isPrime(int num) {
+        if (num < 2) return false;
         
-        for(int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) return false;
+        for (int i = 2; i <= (int) Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
         }
         
         return true;
