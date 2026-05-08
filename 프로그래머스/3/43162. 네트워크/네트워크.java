02ee8707e6bb17c1b47253cb8276b2059e11c1a1
Nageum Jeong <nagum0545@gmail.com>
2026-05-08 12:57:20 +0900

    [level 3] Title: 네트워크, Time: 0.23 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,30 +1,36 @@
 import java.util.*;
 
 class Solution {
+    static boolean visited[];
+    
     public int solution(int n, int[][] computers) {
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] visited = new boolean[n];
-        
+        visited = new boolean[n];
         int answer = 0;
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                visited[i] = true;
-                q.add(i);
+                bfs(i, n, computers);
                 answer++;
-                
-                while (!q.isEmpty()) {
-                    int cur = q.poll(); // 현재 컴퓨터 번호
-                    
-                    for (int nxt = 0; nxt < n; nxt++) {
-                        if (nxt == cur || visited[nxt] || computers[cur][nxt] == 0) continue;
-                        
-                        visited[nxt] = true;
-                        q.add(nxt);
-                    }
-                }
             }
         }
         
         return answer;
+    }
+    
+    static void bfs(int start, int n, int[][] computers) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        visited[start] = true;
+        
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            
+            for (int i = 0; i < n; i++) {
+                if (i == cur) continue;
+                if (!visited[i] && computers[cur][i] == 1) {
+                    q.add(i);
+                    visited[i] = true;
+                }
+            }
+        }
     }
 }
