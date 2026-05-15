4e9584148327201227be606b95d08555fd0e9a3a
Nageum Jeong <nagum0545@gmail.com>
2026-05-15 18:46:58 +0900

    [level 3] Title: 단어 변환, Time: 0.15 ms, Memory: 62 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,44 +1,52 @@
 import java.util.*;
 
 class Solution {
-    static class Word {
-        String word;
-        int cnt;
-        
-        public Word(String s, int cnt) {
-            this.word = s;
-            this.cnt = cnt;
+    public int solution(String begin, String target, String[] words) {
+        // words에 target 유무 확인
+        boolean flag = false;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(target)) flag = true;
         }
+        
+        // 주어진 단어의 집합에 target이 없으면 변환할 수 없음
+        if (!flag) return 0;
+        
+        // 가장 짧은(최소) 변환 과정을 찾아야 하므로 BFS 사용
+        return bfs(begin, target, words);
     }
     
-    public int solution(String begin, String target, String[] words) {
-        Queue<Word> q = new LinkedList<>();
+    static int bfs(String begin, String target, String[] words) {
+        Queue<String[]> q = new LinkedList<>(); // {단어의 인덱스, 변환 횟수}
+        q.add(new String[] {begin, "0"});
         boolean[] visited = new boolean[words.length];
-        q.offer(new Word(begin, 0));
         
-        int answer = 0;
+        int res = 0;
         while (!q.isEmpty()) {
-            Word cur = q.poll();
-            if (cur.word.equals(target)) {
-                answer = cur.cnt;
-                break;
+            String[] cur = q.poll();
+            if (cur[0].equals(target)) { // 현재 단어가 타겟 단어라면 종료
+                res = Integer.parseInt(cur[1]);
+                break; 
             }
             
-            for (int i = 0; i < words.length; i++) {
-                if (!visited[i]) {
-                    int count = 0;
-                    for (int j = 0; j < cur.word.length(); j++) {
-                        if (cur.word.charAt(j) != words[i].charAt(j)) count++;
-                    }
-                    
-                    if (count == 1) {
-                        visited[i] = true;
-                        q.offer(new Word(words[i], cur.cnt+1));
-                    }
-                }
+            for (int nxt = 0; nxt < words.length; nxt++) {
+                if (visited[nxt] || !isChanged(cur[0], words[nxt])) continue;
+                
+                int cnt = Integer.parseInt(cur[1]) + 1;
+                q.add(new String[] {words[nxt], Integer.toString(cnt)});
+                visited[nxt] = true;
             }
         }
         
-        return answer;
+        return res;
+    }
+    
+    static boolean isChanged(String a, String b) {
+        int diff = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) diff++;
+            if (diff > 1) return false;
+        }
+        
+        return diff == 1;
     }
 }
