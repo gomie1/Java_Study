5e650d4c8b23dbe2603ad955f249c95547b27d56
Nageum Jeong <nagum0545@gmail.com>
2026-05-15 17:31:04 +0900

    [level 3] Title: 여행경로, Time: 0.09 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,30 +1,76 @@
 import java.util.*;
 
 class Solution {
-    static ArrayList<String> answer;
-    static boolean[] visited;
+    static Map<String, Integer> num;
+    static int[][] ticketCnt;
+    static List<Integer> path;
+    static List<String> airport;
+    static int n, ticketNum;
+    static String[] answer;
+    static boolean isFound = false;
     
     public String[] solution(String[][] tickets) {
-        answer = new ArrayList<>();
-        visited = new boolean[tickets.length];
+        // 주어진 공항을 리스트에 담기 (중복 X)
+        airport = new ArrayList<>();
+        for (String[] ticket : tickets) {
+            if (!airport.contains(ticket[0])) airport.add(ticket[0]);
+            if (!airport.contains(ticket[1])) airport.add(ticket[1]);
+        }
         
-        dfs("ICN", 0, "ICN", tickets, tickets.length);
-        Collections.sort(answer);
+        // 알파벳 순서가 앞서는 경로부터 가기 위해 정렬
+        Collections.sort(airport);
         
-        return answer.get(0).split(" ");
+        // 공항 별 번호 메기기
+        num = new HashMap<>();
+        n = airport.size();
+        for (int i = 0; i < n; i++) {
+            num.put(airport.get(i), i);
+        }
+        
+        // 티켓에 표기된 대로 방문 가능 여부 표시
+        ticketCnt = new int[n][n];
+        for (String[] ticket : tickets) {
+            int from = num.get(ticket[0]);
+            int to = num.get(ticket[1]);
+            
+            ticketCnt[from][to]++;
+        }
+        
+        path = new ArrayList<>();
+        path.add(num.get("ICN"));
+        ticketNum = tickets.length;
+        dfs(num.get("ICN"), 0);
+        
+        return answer;
     }
     
-    static void dfs(String cur, int cnt, String route, String[][] tickets, int n) {
-        if (cnt == n) {
-            answer.add(route);
+    static void dfs(int cur, int cnt) {
+        // 이미 정답을 찾았다면 즉시 종료
+        if (isFound) return;
+        
+        // 티켓을 모두 사용했다면 종료 (정렬했으니까 가장 처음 조건을 만족한 것이 답이 됨)
+        if (cnt == ticketNum) {
+            answer = new String[path.size()];
+            for (int i = 0; i < path.size(); i++) {
+                answer[i] = airport.get(path.get(i));
+            }
+            
+            isFound = true;
             return;
         }
         
-        for (int i = 0; i < n; i++) {
-            if (!visited[i] && cur.equals(tickets[i][0])) {
-                visited[i] = true;
-                dfs(tickets[i][1], cnt+1, route + " " + tickets[i][1], tickets, n);
-                visited[i] = false;
+        for (int nxt = 0; nxt < n; nxt++) {
+            if (cur == nxt || ticketCnt[cur][nxt] == 0) continue; // 자기 자신은 Pass
+            
+            // 현재 공항에서 다음 공항에 방문할 수 있다면 이동
+            if (ticketCnt[cur][nxt] > 0) {
+                ticketCnt[cur][nxt]--;
+                path.add(nxt);
+                
+                dfs(nxt, cnt+1);
+                
+                path.remove(path.size()-1);
+                ticketCnt[cur][nxt]++;
             }
         }
     }
