fd2676f9d6c41afc87de1d7cef6c821c458f4ced
Nageum Jeong <nagum0545@gmail.com>
2026-05-16 21:26:44 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -3,19 +3,19 @@ import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int rep = (int) Math.sqrt(yellow);
-        for (int row = 1; row <= rep; row++) { // yellow 열 개수 (세로)
-            if (yellow % row != 0) continue;
-            
-            int col = yellow / row; // yellow 행 개수 (가로)
-            int boldCnt = col * 2 + row * 2 + 4;
-            if (brown == boldCnt) {
-                answer[0] = col + 2;
-                answer[1] = row + 2;
+        
+        for (int i = 1; i <= (int) Math.sqrt(yellow); i++) {
+            if (yellow % i == 0 && brown == countBrown(yellow, i)) {
+                answer[0] = (yellow / i) + 2;
+                answer[1] = i + 2;
                 break;
             }
         }
         
         return answer;
+    }
+    
+    static int countBrown(int yellow, int line) {
+        return line * 2 + (yellow / line) * 2 + 4;
     }
 }
