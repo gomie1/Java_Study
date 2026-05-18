899c0b6c605d770c4704d5c9952c4aa89ea6bbfb
Nageum Jeong <nagum0545@gmail.com>
2026-05-18 19:13:21 +0900

    [level 2] Title: 주식가격, Time: 13.27 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -5,21 +5,20 @@ class Solution {
         int n = prices.length;
         int[] answer = new int[n];
         
-        Stack<Integer> stack = new Stack<>(); // sec
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+        Stack<Integer> stack = new Stack<>();
+        for (int sec = 0; sec < n; sec++) {
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[sec]) {
                 int idx = stack.pop();
-                answer[idx] = i - idx; // 떨어진 시점 - 들어온 시점 = 떨어지지 않은 채로 버틴 시간
-                // ex. 2초에서 3이었는데, 3초에서 2로 떨어졌다면 -> 2초 기준에서 1초동안 유지한 것!!
+                answer[idx] = sec - idx;
             }
-            stack.push(i);
+            stack.push(sec);
         }
         
         while (!stack.isEmpty()) {
             int idx = stack.pop();
-            answer[idx] = n - 1 - idx;
+            answer[idx] = (n-1) - idx;
         }
-        
+    
         return answer;
     }
 }
