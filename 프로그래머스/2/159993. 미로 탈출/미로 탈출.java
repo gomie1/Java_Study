db69b6b0b7b67e825b5872b040984e6c7336b168
Nageum Jeong <nagum0545@gmail.com>
2026-05-30 16:48:40 +0900

    [level 2] Title: 미로 탈출, Time: 3.94 ms, Memory: 64.4 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,45 +1,43 @@
 import java.util.*;
 
 class Solution {
-    static int n, m;
-    static char map[][];
-    
     public int solution(String[] maps) {
-        n = maps.length;
-        m = maps[0].length();
+        int n = maps.length;
+        int m = maps[0].length();
         
-        map = new char[n][m];
         int[] start = new int[2];
         int[] end = new int[2];
         int[] lever = new int[2];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                map[i][j] = maps[i].charAt(j);
-                
-                if (map[i][j] == 'S') {
-                    start[0] = i;
-                    start[1] = j;
-                } else if (map[i][j] == 'L') {
-                    lever[0] = i;
-                    lever[1] = j;
-                } else if (map[i][j] == 'E') {
-                    end[0] = i;
-                    end[1] = j;
+        for (int x = 0; x < n; x++) {
+            for (int y = 0; y < m; y++) {
+                if (maps[x].charAt(y) == 'S') {
+                    start[0] = x;
+                    start[1] = y;
+                } else if (maps[x].charAt(y) == 'L') {
+                    lever[0] = x;
+                    lever[1] = y;
+                } else if (maps[x].charAt(y) == 'E') {
+                    end[0] = x;
+                    end[1] = y;
                 }
             }
         }
         
-        int t1 = bfs(start, lever);
-        if (t1 == -1) return -1;
+        int path1 = bfs(start, lever, n, m, maps);
+        if (path1 == -1) return -1; // start to lever에 실패하면 -1 반환 후 종료
         
-        int t2 = bfs(lever, end);
-        return t2 == -1 ? -1 : t1+t2;
+        int path2 = bfs(lever, end, n, m, maps);
+        if (path2 == -1) return -1; // lever to end에 실패하면 -1 반환 후 종료
+        
+        return path1 + path2; // 두 경로 모두 이동 가능하다면 최단 거리를 합산해서 출력
     }
     
-    static int bfs(int[] start, int[] end) {
-        Queue<int[]> q = new LinkedList<>(); // {x, y, time}
-        q.add(new int[] {start[0], start[1], 0});
+    static int bfs(int[] start, int[] end, int n, int m, String[] maps) {
+        Queue<int[]> q = new ArrayDeque<>();
         boolean[][] visited = new boolean[n][m];
+        
+        // 시작 위치를 큐에 삽입 및 방문 처리
+        q.add(new int[] {start[0], start[1], 0}); // [x, y, dist]
         visited[start[0]][start[1]] = true;
         
         int[] dx = {-1, 1, 0, 0};
@@ -52,14 +50,18 @@ class Solution {
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
                 
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m || visited[nx][ny] || map[nx][ny] == 'X') continue;
+                // 다음 위치가 미로를 벗어나거나, 이미 방문했거나, 벽이라면 패스!
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m || visited[nx][ny] || maps[nx].charAt(ny) == 'X') continue;
                 
+                // 다음 위치가 목적지라면 종료!
                 if (nx == end[0] && ny == end[1]) return cur[2] + 1;
+                
+                // 위 조건을 통과한다면, 다음 위치로 이동 및 거리 갱신
                 q.add(new int[] {nx, ny, cur[2]+1});
                 visited[nx][ny] = true;
             }
         }
         
-        return -1;
+        return -1; // 목적지에 도착하지 못했다면 -1 반환
     }
 }
