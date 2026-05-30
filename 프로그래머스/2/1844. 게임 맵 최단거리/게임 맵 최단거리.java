4fb37f37f1a4ddeebb626255ff8b66e7fe2aee63
Nageum Jeong <nagum0545@gmail.com>
2026-05-30 16:23:06 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.63 ms, Memory: 58.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -7,13 +7,16 @@ class Solution {
         n = maps.length;
         m = maps[0].length;
         
-        return bfs(maps);
+        int answer = play(maps);
+        return answer;
     }
     
-    static int bfs(int[][] maps) {
+    static int play(int[][] maps) {
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {0, 0, 1}); // {x, y, dist}
         boolean[][] visited = new boolean[n][m];
+        
+        // 캐릭터의 초기 위치 설정
+        q.add(new int[] {0, 0, 1}); // [x, y, dist]
         visited[0][0] = true;
         
         int[] dx = {-1, 1, 0, 0};
@@ -26,10 +29,13 @@ class Solution {
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
                 
+                // 다음 위치가 맵을 벗어나거나, 이미 방문한 위치이거나, 벽이라면 패스!
                 if (nx < 0 || nx >= n || ny < 0 || ny >= m || visited[nx][ny] || maps[nx][ny] == 0) continue;
                 
-                if (nx == n-1 && ny == m-1) return cur[2] + 1;
+                // 다음 위치가 상대 팀 진영이라면 종료!
+                if (nx == n-1 && ny == m-1) return cur[2]+1;
                 
+                // 위 조건을 통과한다면, 다음 위치로 이동 및 최단 거리 갱신
                 q.add(new int[] {nx, ny, cur[2]+1});
                 visited[nx][ny] = true;
             }
