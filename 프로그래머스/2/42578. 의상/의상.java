1bbd760b21d3f344b558dca92b28f3b8938761d2
Nageum Jeong <nagum0545@gmail.com>
2026-06-05 23:02:30 +0900

    [level 2] Title: 의상, Time: 0.07 ms, Memory: 64.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,21 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        // 1. 종류별 옷 개수 카운팅
+        // 1. map을 사용해 의상 종류별로 의상의 개수 카운팅
         Map<String, Integer> map = new HashMap<>();
         for (String[] c : clothes) {
-            map.put(c[1], map.getOrDefault(c[1], 0) + 1);
+            map.put(c[1], map.getOrDefault(c[1], 1) + 1);
         }
         
-        // 옷 종류가 하나라면 한개씩 밖에 입지 못함
-        if (map.size() == 1) return clothes.length;
-        
-        // 2. 경우의 수 구하기
+        // 2. 모든 경우의 수 구하기
         int answer = 1;
-        for (String c : map.keySet()) {
-            answer *= map.get(c) + 1;
+        for (String key : map.keySet()) {
+            answer *= map.get(key);
         }
         
-        return answer-1;
+        return answer - 1; // 아무것도 입지 않는 경우는 제외한 결과 반환
     }
 }
