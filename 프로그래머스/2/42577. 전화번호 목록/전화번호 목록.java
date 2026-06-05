6fb4244472dbca88f3fff8acaefb7deb6290659e
Nageum Jeong <nagum0545@gmail.com>
2026-06-05 22:22:30 +0900

    [level 2] Title: 전화번호 목록, Time: 208.75 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,9 +2,11 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
+        // 1. 전화번호 정렬하기
+        // (* 전화번호가 String이기 때문에, 정렬하면 접두어가 같은 단어가 이어서 나옴)
         Arrays.sort(phone_book);
         
-        String prev = phone_book[0];
+        // 2. 현재 전화번호와 다음 전화번호를 비교하며, 접두어인 경우를 찾으면 종료
         for (int i = 0; i < phone_book.length-1; i++) {
             if (phone_book[i+1].startsWith(phone_book[i])) return false;
         }
