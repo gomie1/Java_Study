eed22b0ba018a97dce6ccfe7641c1d0539c78548
Nageum Jeong <nagum0545@gmail.com>
2026-06-21 02:25:32 +0900

    [level 2] Title: 올바른 괄호, Time: 8.94 ms, Memory: 58.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,16 +1,19 @@
+import java.util.*;
+
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        int cnt = 0;
+        // 1. 괄호 짝 검사
+        Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
-            if (c == '(') cnt++;
+            if (c == '(') stack.add(c);
             else {
-                if (cnt == 0) return false;
-                else cnt--;
+                if (stack.isEmpty()) return false;
+                stack.pop();
             }
         }
         
-        if (cnt != 0) return false;
-        return answer;
+        // 2. 검사가 끝난 후에 스택이 비어있지 않으면 올바르지 않음
+        if (!stack.isEmpty()) return false;
+        return true;
     }
 }
