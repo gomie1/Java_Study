e065355dc5a83ae38828b1c3761511de29222ce3
Nageum Jeong <nagum0545@gmail.com>
2026-06-25 15:50:55 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,22 @@
+-- 코드를 작성해주세요
+SELECT E.EMP_NO, E.EMP_NAME,
+    CASE
+        WHEN AVG_SCORE >= 96 THEN 'S'
+        WHEN AVG_SCORE >= 90 THEN 'A'
+        WHEN AVG_SCORE >= 80 THEN 'B'
+        ELSE 'C'
+    END AS GRADE, 
+    CASE
+        WHEN AVG_SCORE >= 96 THEN E.SAL * 0.2
+        WHEN AVG_SCORE >= 90 THEN E.SAL * 0.15
+        WHEN AVG_SCORE >= 80 THEN E.SAL * 0.1
+        ELSE 0
+    END AS BONUS
+FROM HR_EMPLOYEES E
+JOIN (
+    -- 사원별 평균 점수 구하기
+    SELECT EMP_NO, AVG(SCORE) AS AVG_SCORE
+    FROM HR_GRADE
+    GROUP BY EMP_NO
+) AS G ON E.EMP_NO = G.EMP_NO
+ORDER BY E.EMP_NO;
