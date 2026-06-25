5bf863d6328c0ee0985867c8d0922f43eae3db29
Nageum Jeong <nagum0545@gmail.com>
2026-06-26 00:39:52 +0900

    [level 3] Title: 징검다리 건너기, Time: 25.01 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -2,35 +2,32 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] stones, int k) {
-        int left = 200000001;
-        int right = 0;
-        for (int stone : stones) {
-            left = Math.min(left, stone);
-            right = Math.max(right, stone);
-        }
+        int left = 1; // 징검다리를 건널 수 있는 최소 인원 수
+        int right = 0; // 징검다리를 건널 수 있는 최대 인원 수 = stones의 최대값 (다 0이 되어서 건널 수 없어짐)
+        for (int stone : stones) right = Math.max(right, stone);
         
+        int answer = 0;
         while (left <= right) {
             int mid = (left + right) / 2;
             
-            int cnt = 0;
-            int max = 0;
+            int jumpCnt = 0;
             for (int stone : stones) {
-                if (stone <= mid) cnt++;
-                else {
-                    if (cnt != 0) {
-                        max = Math.max(max, cnt);
-                        cnt = 0;
-                    }
-                }
+                // 해당 돌을 mid명이 건널 수 없다면 건너뛰기
+                if (stone - mid < 0) jumpCnt++;
+                else jumpCnt = 0; // 해당 돌을 mid명이 건널 수 있다면, 건너뛴 횟수 초기화
+                
+                // k보다 더 많이 건너뛰는 것은 불가능하므로 종료
+                if (jumpCnt == k) break;
             }
             
-            // 끝까지 건너뛰는 경우도 포함하기 위함
-            max = Math.max(max, cnt);
-            
-            if (k <= max) right = mid - 1;
-            else left = mid + 1;
+            // mid명이 건널 수 있다면, 인원 수 늘려보기 (최대 인원수를 구하는 것이므로)
+            if (jumpCnt < k) {
+                answer = mid; // 정답 후보 저장
+                left = mid + 1;
+            }
+            else right = mid - 1; // mid명은 건널 수 없다면, 인원 수 줄이기
         }
         
-        return left;
+        return answer;
     }
 }
