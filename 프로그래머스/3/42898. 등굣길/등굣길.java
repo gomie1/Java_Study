8aa14246194ae0a3ac49911b760247338b68d88a
Nageum Jeong <nagum0545@gmail.com>
2026-06-26 17:45:18 +0900

    [level 3] Title: 등굣길, Time: 0.79 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -2,23 +2,26 @@ import java.util.*;
 
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int[][] dp = new int[n+1][m+1]; // 각 위치별 최소 이동 횟수
-        for (int[] puddle : puddles) {
-            dp[puddle[1]][puddle[0]] = -1;
-        }
+        // dp[i][j]: (i, j)까지 갈 수 있는 방법의 수
+        // -> dp[i][j] = (위쪽에서 오는 방법의 수) + (왼쪽에서 오는 방법의 수)
+        int[][] dp = new int[n][m];
         
-        dp[1][1] = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if ((i == 1 && j == 1) || dp[i][j] == -1) continue;
+        boolean[][] isPossible = new boolean[n][m];
+        for (int[] p : puddles) isPossible[p[1]-1][p[0]-1] = true;
+        
+        dp[0][0] = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if ((i == 0 && j == 0) || isPossible[i][j]) continue;
                 
-                // 현재 위치까지 오는 방법은 왼쪽에서 오는 방법 + 위쪽에서 오는 방법
-                int left = dp[i][j-1] == -1 ? 0 : dp[i][j-1];
-                int top = dp[i-1][j] == -1 ? 0 : dp[i-1][j];
-                dp[i][j] = (left + top) % 1000000007;
+                if (i == 0) dp[i][j] = dp[i][j-1];
+                else if (j == 0) dp[i][j] = dp[i-1][j];
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                
+                dp[i][j] %= 1000000007;
             }
         }
         
-        return dp[n][m];
+        return dp[n-1][m-1] % 1000000007;
     }
 }
