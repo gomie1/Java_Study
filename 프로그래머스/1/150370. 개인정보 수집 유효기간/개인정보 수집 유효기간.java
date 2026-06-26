1c23d9547328e20b5b12913bf40412cb984c95c2
Nageum Jeong <nagum0545@gmail.com>
2026-06-26 12:54:45 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.82 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,53 +1,41 @@
 import java.util.*;
 
+// 시간을 다루는 문제는 보통 초로 변환 후 다루듯이, 날짜도 일로 변환 후 활용!!
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        // 1. terms를 Map으로 저장하기
-        Map<String, Integer> termsMap = new HashMap<>();
+        int n = privacies.length; // 개인정보의 수
+        int todayInt = changeDateType(today);
+        
+        // 유효기간을 일로 변환 후 종류별로 map에 저장
+        Map<String, Integer> map = new HashMap<>();
+        StringTokenizer st;
         for (String term : terms) {
-            String[] str = term.split(" ");
-            termsMap.put(str[0], Integer.parseInt(str[1]));
+            st = new StringTokenizer(term);
+            map.put(st.nextToken(), Integer.parseInt(st.nextToken()) * 28);
         }
         
-        // 2. 오늘 날짜를 String -> Integer로 변경
-        String[] todaySplit = today.split("\\.");
-        int todayInt = Integer.parseInt(todaySplit[0]) * 10000 + Integer.parseInt(todaySplit[1]) * 100 + Integer.parseInt(todaySplit[2]);
-        
-        // 3. 각 개인정보별 만료일 계산 + 파기 여부 결정
-        ArrayList<Integer> ans = new ArrayList<>();
-        for (int i = 0; i < privacies.length; i++) {
-            String[] str = privacies[i].split(" ");
-            int period = termsMap.get(str[1]);
-            
-            String[] strDate = str[0].split("\\.");
-            int date = datePlus(strDate[0], strDate[1], strDate[2], period);
-            
-            if (date < todayInt) ans.add(i+1);
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(privacies[i]);
+            int date = changeDateType(st.nextToken()) + map.get(st.nextToken());
+            if (date <= todayInt) res.add(i+1);
         }
         
-        int[] answer = new int[ans.size()];
-        for (int i = 0; i < ans.size(); i++) {
-            answer[i] = ans.get(i);
+        int[] answer = new int[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            answer[i] = res.get(i);
         }
-        
         return answer;
     }
     
-    // 만료일 계산 함수
-    static int datePlus (String y, String m, String d, int period) {
-        int yearInt = Integer.parseInt(y);
-        int monthInt = Integer.parseInt(m);
-        int dayInt = Integer.parseInt(d);
+    static int changeDateType(String date) {
+        String[] ymd = date.split("\\."); // '.'은 역슬래시로 감싸주어야 인식됨
         
-        if (dayInt == 1) {
-            monthInt--;
-            dayInt = 28;
-        } else dayInt--;
+        // ex. 2022.05.19 -> year = 2021 * 12 * 28, month = 4 * 28, day = 19
+        int year = (Integer.parseInt(ymd[0]) - 1) * 12 * 28;
+        int month = (Integer.parseInt(ymd[1]) - 1) * 28;
+        int day = Integer.parseInt(ymd[2]); // 모든 달은 28일까지 있다고 가정
         
-        monthInt += period;
-        if (monthInt > 12) yearInt += (monthInt - 1) / 12;
-        monthInt = (monthInt - 1) % 12 + 1;
-        
-        return yearInt * 10000 + monthInt * 100 + dayInt;
+        return year + month + day;
     }
 }
