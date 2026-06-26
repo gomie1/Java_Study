393fdacd6717fa9f799a5ecfb33fda5552216fde
Nageum Jeong <nagum0545@gmail.com>
2026-06-26 14:43:08 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.38 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,22 +1,15 @@
 import java.util.*;
 
 class Solution {
-    static int n, m;
-    
     public int solution(int[][] maps) {
-        n = maps.length;
-        m = maps[0].length;
-        
-        int answer = play(maps);
-        return answer;
+        return bfs(maps, maps.length, maps[0].length);
     }
     
-    static int play(int[][] maps) {
-        Queue<int[]> q = new LinkedList<>();
-        boolean[][] visited = new boolean[n][m];
+    static int bfs(int[][] maps, int n, int m) {
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[] {0, 0, 1}); // 시작 위치는 (0, 0), 첫 위치부터 카운팅
         
-        // 캐릭터의 초기 위치 설정
-        q.add(new int[] {0, 0, 1}); // [x, y, dist]
+        boolean[][] visited = new boolean[n][m];
         visited[0][0] = true;
         
         int[] dx = {-1, 1, 0, 0};
@@ -24,18 +17,14 @@ class Solution {
         
         while (!q.isEmpty()) {
             int[] cur = q.poll();
+            if (cur[0] == n-1 && cur[1] == m-1) return cur[2];
             
             for (int i = 0; i < 4; i++) {
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
                 
-                // 다음 위치가 맵을 벗어나거나, 이미 방문한 위치이거나, 벽이라면 패스!
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m || visited[nx][ny] || maps[nx][ny] == 0) continue;
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m || maps[nx][ny] == 0 || visited[nx][ny]) continue;
                 
-                // 다음 위치가 상대 팀 진영이라면 종료!
-                if (nx == n-1 && ny == m-1) return cur[2]+1;
-                
-                // 위 조건을 통과한다면, 다음 위치로 이동 및 최단 거리 갱신
                 q.add(new int[] {nx, ny, cur[2]+1});
                 visited[nx][ny] = true;
             }
