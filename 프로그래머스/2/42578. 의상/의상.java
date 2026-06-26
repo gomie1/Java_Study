21d30900f27f8f105001c69f9e8e1e456b2c5cdf
Nageum Jeong <nagum0545@gmail.com>
2026-06-26 14:18:16 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,17 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> cnt = new HashMap<>();
         for (String[] c : clothes) {
-            // default 값은 안입는 경우를 포함해서 1로 지정
-            map.put(c[1], map.getOrDefault(c[1], 1) + 1); 
+            // 해당 종류의 의상을 입지 않는 경우도 있기에, 초기값을 1로 설정
+            cnt.put(c[1], cnt.getOrDefault(c[1], 1) + 1);
         }
         
         int answer = 1;
-        for (String key : map.keySet()) {
-            answer *= map.get(key);
+        for (String key : cnt.keySet()) {
+            answer *= cnt.get(key);
         }
-        
-        return answer - 1; // 아무것도 안입는 경우 제외
+        return answer - 1; // 아무것도 입지 않는 경우는 제외
     }
 }
