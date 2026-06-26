45282d4dca2d77492af242eb0a9cd28ff3fc68e5
Nageum Jeong <nagum0545@gmail.com>
2026-06-26 14:31:24 +0900

    [level 2] Title: 오픈채팅방, Time: 86.06 ms, Memory: 160 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -2,28 +2,28 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] record) {
-        // 1. ID별 닉네임 저장하기
-        Map<String, String> nickname = new HashMap<>();
+        // 유저 ID별 닉네임 매칭하기
+        Map<String, String> users = new HashMap<>(); // <ID, 닉네임>
         for (String str : record) {
             String[] info = str.split(" ");
-            if (info[0].equals("Leave")) continue;
-            nickname.put(info[1], info[2]);
+            if (info[0].equals("Enter") || info[0].equals("Change")) users.put(info[1], info[2]);
         }
         
-        // 2. 최종 닉네임으로 기록 출력하기
-        List<String> answer = new ArrayList<>();
-        for (int i = 0; i < record.length; i++) {
-            String[] info = record[i].split(" ");
+        // 최종 닉네임으로 결과 출력하기
+        List<String> res = new ArrayList<>();
+        for (String str : record) {
+            String[] info = str.split(" ");
             if (info[0].equals("Change")) continue;
             
-            StringBuilder sb = new StringBuilder();
-            sb.append(nickname.get(info[1]));
-            if (info[0].equals("Enter")) sb.append("님이 들어왔습니다.");
-            if (info[0].equals("Leave")) sb.append("님이 나갔습니다.");
-            
-            answer.add(sb.toString());
+            if (info[0].equals("Enter")) res.add(users.get(info[1]) + "님이 들어왔습니다.");
+            else res.add(users.get(info[1]) + "님이 나갔습니다.");
         }
         
-        return answer.toArray(new String[answer.size()]);
+        String[] answer = new String[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            answer[i] = res.get(i);
+        }
+        
+        return answer;
     }
 }
