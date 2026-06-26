c9b596e1efb25fd61c801a1e0e954f7f406209f9
Nageum Jeong <nagum0545@gmail.com>
2026-06-26 14:35:14 +0900

    [level 2] Title: 타겟 넘버, Time: 3.10 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -5,21 +5,17 @@ class Solution {
     
     public int solution(int[] numbers, int target) {
         answer = 0;
-        dfs(0, 0, target, numbers);
+        dfs(numbers, target, 0, 0);
         return answer;
     }
     
-    static void dfs(int cur, int idx, int target, int[] numbers) {
-        // 모든 숫자에 대해 연산을 했다면 종료
+    static void dfs(int[] numbers, int target, int idx, int sum) {
         if (idx == numbers.length) {
-            if (cur == target) answer++;
+            if (sum == target) answer++;
             return;
         }
         
-        // 현재 숫자 더하기
-        dfs(cur+numbers[idx], idx+1, target, numbers);
-        
-        // 현재 숫자 빼기
-        dfs(cur-numbers[idx], idx+1, target, numbers);
+        dfs(numbers, target, idx+1, sum+numbers[idx]); // 더하는 경우
+        dfs(numbers, target, idx+1, sum-numbers[idx]); // 빼하는 경우
     }
 }
